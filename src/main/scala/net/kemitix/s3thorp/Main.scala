a79c3320a51383ae40849166ca846ee245eda611
Paul Campbell <pcampbell@kemitix.net>
2019-04-29 20:24:10 +0100

    [main] Convert app to cats-effect IOApp

--- src/main/scala/net/kemitix/s3thorp/Main.scala
+++ src/main/scala/net/kemitix/s3thorp/Main.scala
@@ -1,9 +1,11 @@
 package net.kemitix.s3thorp
 
-object Main {
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.syntax.all._
 
-  def main(args: Array[String]): Unit = {
-    println("S3Thorp - hashed sync for s3")
-  }
+object Main extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] =
+    IO(println("S3Thorp - hashed sync for s3")).as(ExitCode.Success)
 
 }
