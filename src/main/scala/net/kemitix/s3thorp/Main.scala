73f4a7925470d560923d84d1a3fd2dd7b7ba9c98
Paul Campbell <pcampbell@kemitix.net>
2019-04-29 20:28:02 +0100

    [main] Report Done or Interrupted on exit

--- src/main/scala/net/kemitix/s3thorp/Main.scala
+++ src/main/scala/net/kemitix/s3thorp/Main.scala
@@ -1,11 +1,18 @@
 package net.kemitix.s3thorp
 
+import cats.effect.ExitCase.Canceled
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.syntax.all._
 
 object Main extends IOApp {
 
-  override def run(args: List[String]): IO[ExitCode] =
+  def exec(args: List[String]): IO[ExitCode] =
     IO(println("S3Thorp - hashed sync for s3")).as(ExitCode.Success)
+
+  override def run(args: List[String]): IO[ExitCode] =
+    exec(args).guaranteeCase {
+      case Canceled => IO(println("Interrupted"))
+      case _ => IO(println("Done"))
+    }
 
 }
