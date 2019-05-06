b5e1036874f24b2ad00e7dcffeda1d9c88fa583f
Paul Campbell <pcampbell@kemitix.net>
2019-05-06 10:32:53 +0100

    [main] extract IO program

--- src/main/scala/net/kemitix/s3thorp/Main.scala
+++ src/main/scala/net/kemitix/s3thorp/Main.scala
@@ -2,16 +2,21 @@ package net.kemitix.s3thorp
 
 import cats.effect.ExitCase.{Canceled, Completed, Error}
 import cats.effect.{ExitCode, IO, IOApp}
-import cats.syntax.all._
 
 object Main extends IOApp {
 
+  def parseArgs(args: List[String]): IO[Config] = IO.pure(Config("", ""))
+
+  def program(args: List[String]): IO[ExitCode] = for {
+    a <- parseArgs(args)
+    _ <- S3Thorp(a)
+  } yield ExitCode.Success
+
   override def run(args: List[String]): IO[ExitCode] =
-    (for {
-      ec <- S3Thorp(args).as(ExitCode.Success)
-    } yield ec).guaranteeCase {
+    program(args)
+      .guaranteeCase {
         case Canceled => IO(println("Interrupted"))
-        case Error(e) => IO(println("ERROR: " + e))
+        case Error(e) => IO(println("ERROR: " + e.getMessage))
         case Completed => IO(println("Done"))
       }
 
