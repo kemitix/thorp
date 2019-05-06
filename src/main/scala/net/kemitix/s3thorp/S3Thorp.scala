b16862ea20ca98d00567a457099ff576653bd9b2
Paul Campbell <pcampbell@kemitix.net>
2019-05-06 10:32:16 +0100

    [app] S3Throp expects Config as parameter

--- src/main/scala/net/kemitix/s3thorp/S3Thorp.scala
+++ src/main/scala/net/kemitix/s3thorp/S3Thorp.scala
@@ -4,9 +4,9 @@ import cats.effect.IO
 
 object S3Thorp {
 
-  def putStrLn(value: String): IO[Unit] = IO(println(value))
+  def putStrLn(value: String) = IO { println(value) }
 
-  def apply(args: List[String]): IO[Unit] = {
+  def apply(args: Config): IO[Unit] = {
     for {
       _ <- putStrLn("S3Thorp - hashed sync for s3")
     } yield ()
