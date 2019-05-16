74afb288ccf0221dd426de083883f9452dea3634
Paul Campbell <pcampbell@kemitix.net>
2019-05-16 19:59:06 +0100

    [localfilestream] Compare test files within a Set (#11)
    Fixes #10 
    
    * [localfilestream] Compare test files within a Set
    
    Removes issue of files being read in different orders.
    
    * [localfilestream] add missing parameter type

--- src/test/scala/net/kemitix/s3thorp/LocalFileStreamSuite.scala
+++ src/test/scala/net/kemitix/s3thorp/LocalFileStreamSuite.scala
@@ -1,5 +1,7 @@
 package net.kemitix.s3thorp
 
+import java.io.File
+
 import org.scalatest.FunSpec
 
 class LocalFileStreamSuite extends FunSpec with LocalFileStream {
@@ -7,9 +9,9 @@ class LocalFileStreamSuite extends FunSpec with LocalFileStream {
   describe("streamDirectoryPaths") {
     var uploadResource = Resource(this, "upload")
     it("should find all files") {
-      val result: List[String] = streamDirectoryPaths(uploadResource).toList
-          .map(x=>uploadResource.toPath.relativize(x.toPath).toString)
-      assertResult(List("subdir/leaf-file", "root-file"))(result)
+      val result: Set[String] = streamDirectoryPaths(uploadResource).toSet
+          .map { x: File => uploadResource.toPath.relativize(x.toPath).toString }
+      assertResult(Set("subdir/leaf-file", "root-file"))(result)
     }
   }
 }
