c112ab531895c1cd49b2b7aee478bde8307d8e42
Paul Campbell <pcampbell@kemitix.net>
2019-05-18 11:36:33 +0100

    [ActionGenerator] refactored and removed logging

--- src/main/scala/net/kemitix/s3thorp/ActionGenerator.scala
+++ src/main/scala/net/kemitix/s3thorp/ActionGenerator.scala
@@ -1,21 +1,25 @@
 package net.kemitix.s3thorp
 
+import java.io.File
+
 trait ActionGenerator
-  extends MD5HashGenerator
-  with Logging {
+  extends MD5HashGenerator {
 
   def createActions(s3MetaData: S3MetaData)
                    (implicit c: Config): Stream[Action] =
     s3MetaData match {
-      case S3MetaData(localFile, None) => {
-        log5(s"   Created: ${c.relativePath(localFile)}")(c)
-        Stream(ToUpload(localFile))
-      }
-      case S3MetaData(localFile, Some((remoteKey, remoteHash, lastModified))) =>
-        if (md5File(localFile) != remoteHash) {
-          log5(s"   Updated: ${c.relativePath(localFile)}")(c)
-          Stream(ToUpload(localFile))
-        }
-        else Stream.empty
+
+      // There is a local file, but nothing matching in S3 - Upload
+      case S3MetaData(localFile, None) => uploadFile(localFile)
+
+      // There is a local file and an s3 file with the same name, but different content - Upload
+      case S3MetaData(localFile, Some((_, remoteHash, _)))
+        if md5File(localFile) != remoteHash => uploadFile(localFile)
+
+      case _ => Stream.empty
     }
+
+  private def uploadFile(localFile: File)= {
+    Stream(ToUpload(localFile))
+  }
 }
