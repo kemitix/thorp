9fafc7e462e798ebff4c6e16ac2ad574a10b16b0
Paul Campbell <pcampbell@kemitix.net>
2019-05-19 19:28:10 +0100

    [ActionGeneratorSuite] rename tests and note missing tests

--- src/test/scala/net/kemitix/s3thorp/ActionGeneratorSuite.scala
+++ src/test/scala/net/kemitix/s3thorp/ActionGeneratorSuite.scala
@@ -17,17 +17,10 @@ class ActionGeneratorSuite
       val localFile = LocalFile(resource, config.source, fileToKey)
       val lastModified = LastModified(Instant.now)
       val otherKey = RemoteKey("prefix/other-file")
-      def invokeSubject(input: S3MetaData) =
-        createActions(input).toList
-      describe("#1 remote key exists, hash dpes not match, hash of other keys match") {
-        val input = S3MetaData(localFile,
-          matchByHash = Set(RemoteMetaData(otherKey, localHash, lastModified)),
-          matchByKey = Some(RemoteMetaData(localFile.remoteKey, MD5Hash("previous-hash"), lastModified)))
-        it("copy from other key") {
-          assertResult(List(ToCopy(otherKey, localHash, localFile.remoteKey)))(invokeSubject(input))
-        }
-      }
-      describe("#5 remote key is missing, hash of other keys match") {
+      def invokeSubject(input: S3MetaData) = createActions(input).toList
+
+      describe("#1 local exists, remote exists, remote matches - do nothing") {pending}
+      describe("#2 local exists, remote is missing, other matches - copy") {
         val input = S3MetaData(localFile,
           matchByHash = Set(RemoteMetaData(otherKey, localHash, lastModified)),
           matchByKey = None)
@@ -35,12 +28,22 @@ class ActionGeneratorSuite
           assertResult(List(ToCopy(otherKey, localHash, localFile.remoteKey)))(invokeSubject(input))
         }
       }
-      describe("#6 remote key is missing, hash of other keys do not match") {
+      describe("#3 local exists, remote is missing, other no matches - upload") {
         val input = S3MetaData(localFile, Set.empty, None)
         it("upload") {
           assertResult(List(ToUpload(localFile)))(invokeSubject(input))
         }
       }
+      describe("#4 local exists, remote exists, remote no match, other matches - copy") {
+        val input = S3MetaData(localFile,
+          matchByHash = Set(RemoteMetaData(otherKey, localHash, lastModified)),
+          matchByKey = Some(RemoteMetaData(localFile.remoteKey, MD5Hash("previous-hash"), lastModified)))
+        it("copy from other key") {
+          assertResult(List(ToCopy(otherKey, localHash, localFile.remoteKey)))(invokeSubject(input))
+        }
+      }
+      describe("#5 local exists, remote exists, remote no match, other no matches - upload") {pending}
+      describe("#6 local missing, remote exists - delete") {pending}
     }
   }
 }
