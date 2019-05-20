ac5e4cc51ea22be87fd523ba3e0dc19e4e319749
Paul Campbell <pcampbell@kemitix.net>
2019-05-20 08:39:25 +0100

    [Action] Add ToDelete case class

--- src/main/scala/net/kemitix/s3thorp/Action.scala
+++ src/main/scala/net/kemitix/s3thorp/Action.scala
@@ -5,3 +5,4 @@ case class ToUpload(localFile: LocalFile) extends Action
 case class ToCopy(sourceKey: RemoteKey,
                   hash: MD5Hash,
                   targetKey: RemoteKey) extends Action
+case class ToDelete(remoteKey: RemoteKey) extends Action
