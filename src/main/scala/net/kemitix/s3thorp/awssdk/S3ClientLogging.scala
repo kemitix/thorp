2a7aff36ea80572184a2b590fa4ee474ce4b5b64
Paul Campbell <pcampbell@kemitix.net>
2019-05-23 17:57:31 +0100

    [awssdk] Extract logging into S3ClientLogging

--- src/main/scala/net/kemitix/s3thorp/awssdk/S3ClientLogging.scala
+++ src/main/scala/net/kemitix/s3thorp/awssdk/S3ClientLogging.scala
@@ -1,16 +1,79 @@
 package net.kemitix.s3thorp.awssdk
 
-import net.kemitix.s3thorp.{Bucket, Config, LocalFile, Logging}
+import cats.effect.IO
+import net.kemitix.s3thorp.{Bucket, Config, LocalFile, Logging, RemoteKey}
+import software.amazon.awssdk.services.s3.model.{CopyObjectResponse, DeleteObjectResponse, ListObjectsV2Response, PutObjectResponse}
 
 trait S3ClientLogging
   extends Logging {
 
-  def logUploadStart(localFile: LocalFile, bucket: Bucket)
-                    (implicit c: Config)=
-    log5(s"s3Client:upload:start: ${localFile.file}")
+  def logListObjectsStart(bucket: Bucket,
+                          prefix: RemoteKey)
+                         (implicit c: Config): ListObjectsV2Response => IO[ListObjectsV2Response] = {
+    in => IO {
+      log5(s"listObjects:start:${bucket.name}:${prefix.key}")
+      in
+    }
+  }
 
-  def logUploadDone(localFile: LocalFile, bucket: Bucket)
-                   (implicit c: Config) =
-    log5(s"s3Client:upload:done : ${localFile.file}")
+  def logListObjectsFinish(bucket: Bucket,
+                           prefix: RemoteKey)
+                          (implicit c: Config): ListObjectsV2Response => IO[Unit] = {
+    in => IO {
+      log5(s"listObjects:finish:${bucket.name}:${prefix.key}")
+    }
+  }
 
+  def logUploadStart(localFile: LocalFile,
+                     bucket: Bucket)
+                    (implicit c: Config): PutObjectResponse => IO[PutObjectResponse] = {
+    in => IO {
+      log5(s"upload:start:${localFile.file.length}:${bucket.name}:${localFile.remoteKey}")
+      in
+    }
+  }
+
+  def logUploadFinish(localFile: LocalFile,
+                      bucket: Bucket)
+                     (implicit c: Config): PutObjectResponse => IO[Unit] = {
+    in =>IO {
+      log5(s"upload:finish:${localFile.file.length}:${bucket.name}:${localFile.remoteKey}")
+    }
+  }
+
+  def logCopyStart(bucket: Bucket,
+                   sourceKey: RemoteKey,
+                   targetKey: RemoteKey)
+                  (implicit c: Config): CopyObjectResponse => IO[CopyObjectResponse] = {
+    in => IO {
+      log5(s"copy:start:${bucket.name}:${sourceKey.key}:${targetKey.key}")
+      in
+    }
+  }
+
+  def logCopyFinish(bucket: Bucket,
+                    sourceKey: RemoteKey,
+                    targetKey: RemoteKey)
+                   (implicit c: Config): CopyObjectResponse => IO[Unit] = {
+    in => IO {
+      log5(s"copy:finish:${bucket.name}:${sourceKey.key}:${targetKey.key}")
+    }
+  }
+
+  def logDeleteStart(bucket: Bucket,
+                     remoteKey: RemoteKey)
+                    (implicit c: Config): DeleteObjectResponse => IO[DeleteObjectResponse] = {
+    in => IO {
+      log5(s"delete:start:${bucket.name}:${remoteKey.key}")
+      in
+    }
+  }
+
+  def logDeleteFinish(bucket: Bucket,
+                      remoteKey: RemoteKey)
+                     (implicit c: Config): DeleteObjectResponse => IO[Unit] = {
+    in => IO {
+      log5(s"delete:finish:${bucket.name}:${remoteKey.key}")
+    }
+  }
 }
