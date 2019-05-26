6fb343f6645a32c0d50fbc61de8ca1d8ce6042e0
Paul Campbell <pcampbell@kemitix.net>
2019-05-26 15:19:43 +0100

    [S3ClientMultipartuploaderLogging] refactoring/rewriting strings

--- src/main/scala/net/kemitix/s3thorp/awssdk/S3ClientMultiPartUploaderLogging.scala
+++ src/main/scala/net/kemitix/s3thorp/awssdk/S3ClientMultiPartUploaderLogging.scala
@@ -6,51 +6,54 @@ import software.amazon.awssdk.services.s3.model.{CreateMultipartUploadResponse, 
 trait S3ClientMultiPartUploaderLogging
   extends S3ClientLogging {
 
+  private val prefix = "multi-part upload"
+
   def logMultiPartUploadStart(localFile: LocalFile,
                               tryCount: Int)
                              (implicit c: Config): Unit =
-    log4(s"multi-part upload:try $tryCount: ${localFile.file.getPath}")
+    log4(s"$prefix:upload:try $tryCount: ${localFile.remoteKey}")
 
   def logMultiPartUploadInitiate(localFile: LocalFile)
                                 (implicit c: Config): Unit =
-    log5(s"multi-part upload:initiating: ${localFile.file.getPath}")
+    log5(s"$prefix:initiating: ${localFile.remoteKey}")
 
   def logMultiPartUploadPartDetails(localFile: LocalFile,
                                     nParts: Int,
                                     partSize: Long)
                                    (implicit c: Config): Unit =
-    log5(s"multi-part upload:$nParts parts:$partSize each: ${localFile.file.getPath}")
+    log5(s"$prefix:parts $nParts:each $partSize: ${localFile.remoteKey}")
 
   def logMultiPartUploadPart(localFile: LocalFile,
                              partRequest: UploadPartRequest)
                             (implicit c: Config): Unit =
-    log5(s"multi-part upload:sending part ${partRequest.partNumber}: ${localFile.file.getPath}")
+    log5(s"$prefix:sending:part ${partRequest.partNumber}: ${localFile.remoteKey}")
 
   def logMultiPartUploadPartError(localFile: LocalFile,
                                   partRequest: UploadPartRequest,
                                   error: Throwable)
                                  (implicit c: Config): Unit =
-    warn(s"multi-part upload:part ${partRequest.partNumber}: ${localFile.file.getPath}")
+    warn(s"$prefix:error:part ${partRequest.partNumber}: ${localFile.remoteKey}")
 
   def logMultiPartUploadCompleted(createUploadResponse: CreateMultipartUploadResponse,
                                   uploadPartResponses: Stream[UploadPartResponse],
                                   localFile: LocalFile)
                                  (implicit c: Config): Unit =
-    log4(s"multi-part upload:completed:${uploadPartResponses.size} parts: ${localFile.file.getPath}")
+    log4(s"$prefix:completed:parts ${uploadPartResponses.size}: ${localFile.remoteKey}")
 
   def logMultiPartUploadCancelling(localFile: LocalFile)
                                   (implicit c: Config): Unit =
-    warn(s"multi-part upload:cancelling: ${localFile.file.getPath}")
+    warn(s"$prefix:cancelling: ${localFile.remoteKey}")
 
   def logErrorRetrying(e: Throwable, localFile: LocalFile, tryCount: Int)
                       (implicit c: Config): Unit =
-    warn(s"multi-part upload:try $tryCount:${e.getMessage}: ${localFile.file.getPath}")
+    warn(s"$prefix:retry:error ${e.getMessage}: ${localFile.remoteKey}")
 
   def logErrorCancelling(e: Throwable, localFile: LocalFile)
                         (implicit c: Config) : Unit =
-    error(s"multi-part upload:cancelling:${e.getMessage}: ${localFile.file.getPath}")
+    error(s"$prefix:cancelling:error ${e.getMessage}: ${localFile.remoteKey}")
 
   def logErrorUnknown(e: Throwable, localFile: LocalFile)
                      (implicit c: Config): Unit =
-    error(s"multi-part upload:unknown error:$e: ${localFile.file.getPath}")
+    error(s"$prefix:unknown:error $e: ${localFile.remoteKey}")
+
 }
