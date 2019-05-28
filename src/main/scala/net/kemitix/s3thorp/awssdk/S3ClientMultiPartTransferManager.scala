002009e32fb18fbf787284cb0a8b57ff7641e06e
Paul Campbell <pcampbell@kemitix.net>
2019-05-28 17:24:54 +0100

    [S3ClientMultiPartTransferManager] use request object

--- src/main/scala/net/kemitix/s3thorp/awssdk/S3ClientMultiPartTransferManager.scala
+++ src/main/scala/net/kemitix/s3thorp/awssdk/S3ClientMultiPartTransferManager.scala
@@ -1,5 +1,6 @@
 package net.kemitix.s3thorp.awssdk
 import cats.effect.IO
+import com.amazonaws.services.s3.model.PutObjectRequest
 import com.amazonaws.services.s3.transfer.TransferManager
 import net.kemitix.s3thorp._
 
@@ -16,9 +17,10 @@ class S3ClientMultiPartTransferManager(transferManager: TransferManager)
              bucket: Bucket,
              tryCount: Int)
             (implicit c: Config): IO[S3Action] = {
+    val putObjectRequest = new PutObjectRequest(bucket.name, localFile.remoteKey.key, localFile.file)
     IO {
       logMultiPartUploadStart(localFile, tryCount)
-      val result = transferManager.upload(bucket.name, localFile.remoteKey.key, localFile.file)
+      val result = transferManager.upload(putObjectRequest)
         .waitForUploadResult()
       logMultiPartUploadFinished(localFile)
       UploadS3Action(RemoteKey(result.getKey), MD5Hash(result.getETag))
