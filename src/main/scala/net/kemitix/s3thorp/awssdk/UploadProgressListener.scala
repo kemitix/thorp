a88d33dd50da09a033a5acd7c8f84599e7fb697e
Paul Campbell <pcampbell@kemitix.net>
2019-05-29 21:30:43 +0100

    [UploadProgress{Listener,Logging}: add initial implementations

---
+++ src/main/scala/net/kemitix/s3thorp/awssdk/UploadProgressListener.scala
@@ -0,0 +1,17 @@
+package net.kemitix.s3thorp.awssdk
+
+import com.amazonaws.event.{ProgressEvent, ProgressListener}
+import net.kemitix.s3thorp.{Config, LocalFile}
+
+class UploadProgressListener(localFile: LocalFile)
+  (implicit c: Config)
+  extends UploadProgressLogging {
+
+  def create: ProgressListener = new ProgressListener {
+    override def progressChanged(progressEvent: ProgressEvent): Unit = {
+      val eventType = progressEvent.getEventType
+      if (eventType.isTransferEvent) logTransfer(localFile, eventType)
+      else logRequestCycle(localFile, eventType, progressEvent.getBytes, progressEvent.getBytesTransferred)
+    }
+  }
+}
