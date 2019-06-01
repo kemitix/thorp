5f18b3c20c94c9471fe21d1fadac389c4b1ef847
Paul Campbell <pcampbell@kemitix.net>
2019-06-01 22:22:08 +0100

    [LocalFile] allow overriding Hash

--- src/main/scala/net/kemitix/s3thorp/LocalFile.scala
+++ src/main/scala/net/kemitix/s3thorp/LocalFile.scala
@@ -6,13 +6,14 @@ import java.nio.file.Path
 final case class LocalFile(
   file: File,
   source: File,
-  keyGenerator: File => RemoteKey)
+  keyGenerator: File => RemoteKey,
+  suppliedHash: Option[MD5Hash] = None)
   (implicit c: Config)
   extends MD5HashGenerator {
 
   require(!file.isDirectory, s"LocalFile must not be a directory: $file")
 
-  private lazy val myhash = md5File(file)
+  private lazy val myhash = suppliedHash.getOrElse(md5File(file))
 
   def hash: MD5Hash = myhash
 
