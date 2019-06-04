7693f57fcc315ae167392683f2ed53b2919b1218
Paul Campbell <pcampbell@kemitix.net>
2019-06-04 07:57:33 +0100

    [cli] copy in Logging trait as Logger class

---
+++ cli/src/main/scala/net/kemitix/s3thorp/cli/Logger.scala
@@ -0,0 +1,22 @@
+package net.kemitix.s3thorp.cli
+
+import com.typesafe.scalalogging.LazyLogging
+import net.kemitix.s3thorp.domain.Config
+
+class Logger extends LazyLogging {
+
+  def log1(message: String)(implicit config: Config): Unit = if (config.verbose >= 1) logger.info(s"1:$message")
+
+  def log2(message: String)(implicit config: Config): Unit = if (config.verbose >= 2) logger.info(s"2:$message")
+
+  def log3(message: String)(implicit config: Config): Unit = if (config.verbose >= 3) logger.info(s"3:$message")
+
+  def log4(message: String)(implicit config: Config): Unit = if (config.verbose >= 4) logger.info(s"4:$message")
+
+  def log5(message: String)(implicit config: Config): Unit = if (config.verbose >= 5) logger.info(s"5:$message")
+
+  def warn(message: String): Unit = logger.warn(message)
+
+  def error(message: String): Unit = logger.error(message)
+
+}
