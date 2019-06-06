478e70a5d723e79e6b3aa047a0f70ed2358595d8
Paul Campbell <pcampbell@kemitix.net>
2019-06-06 19:38:49 +0100

    [domain] remove redundant braces after class definition

--- domain/src/main/scala/net/kemitix/s3thorp/domain/LastModified.scala
+++ domain/src/main/scala/net/kemitix/s3thorp/domain/LastModified.scala
@@ -2,6 +2,4 @@ package net.kemitix.s3thorp.domain
 
 import java.time.Instant
 
-final case class LastModified(when: Instant) {
-
-}
+final case class LastModified(when: Instant)
--- domain/src/main/scala/net/kemitix/s3thorp/domain/RemoteMetaData.scala
+++ domain/src/main/scala/net/kemitix/s3thorp/domain/RemoteMetaData.scala
@@ -2,6 +2,4 @@ package net.kemitix.s3thorp.domain
 
 final case class RemoteMetaData(remoteKey: RemoteKey,
                                 hash: MD5Hash,
-                                lastModified: LastModified) {
-
-}
+                                lastModified: LastModified)
