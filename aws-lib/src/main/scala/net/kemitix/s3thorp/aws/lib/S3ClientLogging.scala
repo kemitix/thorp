9512f98b4f55402a287b815419b9fa837ab8fdd9
Paul Campbell <pcampbell@kemitix.net>
2019-06-07 20:41:03 +0100

    [aws-lib] Client Logging remove redundant braces

--- aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientLogging.scala
+++ aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientLogging.scala
@@ -8,65 +8,63 @@ object S3ClientLogging {
 
   def logListObjectsStart(bucket: Bucket,
                           prefix: RemoteKey)
-                         (implicit info: Int => String => Unit): ListObjectsV2Result => IO[ListObjectsV2Result] = {
+                         (implicit info: Int => String => Unit): ListObjectsV2Result => IO[ListObjectsV2Result] =
     in => IO {
       info(3)(s"Fetch S3 Summary: ${bucket.name}:${prefix.key}")
       in
     }
-  }
 
   def logListObjectsFinish(bucket: Bucket,
                            prefix: RemoteKey)
-                          (implicit info: Int => String => Unit): ListObjectsV2Result => IO[Unit] = {
-    in => IO {
+                          (implicit info: Int => String => Unit): ListObjectsV2Result => IO[Unit] =
+    _ => IO {
       info(2)(s"Fetched S3 Summary: ${bucket.name}:${prefix.key}")
     }
-  }
 
   def logUploadStart(localFile: LocalFile,
                      bucket: Bucket)
-                    (implicit info: Int => String => Unit): PutObjectResult => IO[PutObjectResult] = {
+                    (implicit info: Int => String => Unit): PutObjectResult => IO[PutObjectResult] =
     in => IO {
       info(4)(s"Uploading: ${bucket.name}:${localFile.remoteKey.key}")
       in
     }
-  }
 
   def logUploadFinish(localFile: LocalFile,
                       bucket: Bucket)
-                     (implicit info: Int => String => Unit): PutObjectResult => IO[Unit] = {
-    in =>IO {
+                     (implicit info: Int => String => Unit): PutObjectResult => IO[Unit] =
+    _ => IO {
       info(1)(s"Uploaded: ${bucket.name}:${localFile.remoteKey.key}")
     }
-  }
 
   def logCopyStart(bucket: Bucket,
                    sourceKey: RemoteKey,
                    targetKey: RemoteKey)
-                  (implicit info: Int => String => Unit): CopyObjectResult => IO[CopyObjectResult] = {
+                  (implicit info: Int => String => Unit): CopyObjectResult => IO[CopyObjectResult] =
     in => IO {
       info(4)(s"Copy: ${bucket.name}:${sourceKey.key} => ${targetKey.key}")
       in
     }
-  }
 
   def logCopyFinish(bucket: Bucket,
                     sourceKey: RemoteKey,
                     targetKey: RemoteKey)
-                   (implicit info: Int => String => Unit): CopyObjectResult => IO[Unit] = {
-    in => IO {
+                   (implicit info: Int => String => Unit): CopyObjectResult => IO[Unit] =
+    _ => IO {
       info(3)(s"Copied: ${bucket.name}:${sourceKey.key} => ${targetKey.key}")
     }
-  }
 
   def logDeleteStart(bucket: Bucket,
                      remoteKey: RemoteKey)
                     (implicit info: Int => String => Unit): IO[Unit] =
-    IO{info(4)(s"Delete: ${bucket.name}:${remoteKey.key}")}
+    IO {
+      info(4)(s"Delete: ${bucket.name}:${remoteKey.key}")
+    }
 
   def logDeleteFinish(bucket: Bucket,
                       remoteKey: RemoteKey)
                      (implicit info: Int => String => Unit): IO[Unit] =
-    IO {info(3)(s"Deleted: ${bucket.name}:${remoteKey.key}")}
+    IO {
+      info(3)(s"Deleted: ${bucket.name}:${remoteKey.key}")
+    }
 
 }
