ef726298d8d2b8a1bcd6b661665d5e15fed11a7e
Paul Campbell <pcampbell@kemitix.net>
2019-06-09 23:10:24 +0100

    [aws-lib] TransferManager displays log messages
    Use the UploadProgressListener that was being ignored, and use
    unsafeRunSync to execute the suspended effect within the IO[Unit].
    Using unsafeRunSync is required to render the effects as the listener
    returns Unit, meaning the suspended effects would be discarded.

--- aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientTransferManager.scala
+++ aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientTransferManager.scala
@@ -6,7 +6,7 @@ import com.amazonaws.services.s3.model.PutObjectRequest
 import com.amazonaws.services.s3.transfer.TransferManager
 import net.kemitix.s3thorp.aws.api.S3Action.UploadS3Action
 import net.kemitix.s3thorp.aws.api.UploadEvent.{ByteTransferEvent, RequestEvent, TransferEvent}
-import net.kemitix.s3thorp.aws.api.{S3Action, UploadProgressListener}
+import net.kemitix.s3thorp.aws.api.{S3Action, UploadEvent, UploadProgressListener}
 import net.kemitix.s3thorp.aws.lib.S3ClientTransferManagerLogging.{logMultiPartUploadFinished, logMultiPartUploadStart}
 import net.kemitix.s3thorp.domain.{Bucket, LocalFile, MD5Hash, RemoteKey}
 
@@ -24,25 +24,35 @@ class S3ClientTransferManager(transferManager: => TransferManager) {
              maxRetries: Int)
             (implicit info: Int => String => IO[Unit],
              warn: String => IO[Unit]): IO[S3Action] = {
-    val putObjectRequest: PutObjectRequest =
-      new PutObjectRequest(bucket.name, localFile.remoteKey.key, localFile.file)
-        .withGeneralProgressListener(progressListener(uploadProgressListener))
     for {
       _ <- logMultiPartUploadStart(localFile, tryCount)
+      listener = progressListener(uploadProgressListener)
+      putObjectRequest = request(localFile, bucket, listener)
       upload = transferManager.upload(putObjectRequest)
       result <- IO{upload.waitForUploadResult}
       _ <- logMultiPartUploadFinished(localFile)
     } yield UploadS3Action(RemoteKey(result.getKey), MD5Hash(result.getETag))
   }
 
-  def progressListener(uploadProgressListener: UploadProgressListener): ProgressListener =
-    {
-      case e: ProgressEvent if isTransfer(e) =>
-        TransferEvent(e.getEventType.name)
-      case e: ProgressEvent if isByteTransfer(e) =>
-        ByteTransferEvent(e.getEventType.name)
-      case e: ProgressEvent =>
-        RequestEvent(e.getEventType.name, e.getBytes, e.getBytesTransferred)
+  private def request(localFile: LocalFile, bucket: Bucket, listener: ProgressListener): PutObjectRequest = {
+    new PutObjectRequest(bucket.name, localFile.remoteKey.key, localFile.file)
+      .withGeneralProgressListener(listener)
+  }
+
+  private def progressListener(uploadProgressListener: UploadProgressListener) =
+    new ProgressListener {
+      override def progressChanged(progressEvent: ProgressEvent): Unit = {
+        uploadProgressListener.listener(
+          progressEvent match {
+            case e: ProgressEvent if isTransfer(e) =>
+              TransferEvent(e.getEventType.name)
+            case e: ProgressEvent if isByteTransfer(e) =>
+              ByteTransferEvent(e.getEventType.name)
+            case e: ProgressEvent =>
+              RequestEvent(e.getEventType.name, e.getBytes, e.getBytesTransferred)
+          })
+          .unsafeRunSync // the listener doesn't execute otherwise as it is never returned
+      }
     }
 
   private def isTransfer(e: ProgressEvent) =
