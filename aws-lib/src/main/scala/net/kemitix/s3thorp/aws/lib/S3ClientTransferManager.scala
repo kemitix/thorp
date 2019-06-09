f1a0b23d4d0bc58fc6bfd6c0460b3f3ee5db07f5
Paul Campbell <pcampbell@kemitix.net>
2019-06-09 18:51:19 +0100

    [aws-lib] TransferManager refactor

--- aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientTransferManager.scala
+++ aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientTransferManager.scala
@@ -35,15 +35,12 @@ class S3ClientTransferManager(transferManager: => TransferManager) {
     } yield UploadS3Action(RemoteKey(result.getKey), MD5Hash(result.getETag))
   }
 
-
   def progressListener(uploadProgressListener: UploadProgressListener): ProgressListener = {
-    new ProgressListener {
-      override def progressChanged(event: ProgressEvent): Unit = {
-        event match {
-          case e if e.getEventType.isTransferEvent => TransferEvent(e.getEventType.name)
-          case e if e.getEventType equals ProgressEventType.RESPONSE_BYTE_TRANSFER_EVENT => ByteTransferEvent(e.getEventType.name)
-          case e => RequestEvent(e.getEventType.name, e.getBytes, e.getBytesTransferred)
-        }
+    event: ProgressEvent => {
+      event match {
+        case e if e.getEventType.isTransferEvent => TransferEvent(e.getEventType.name)
+        case e if e.getEventType equals ProgressEventType.RESPONSE_BYTE_TRANSFER_EVENT => ByteTransferEvent(e.getEventType.name)
+        case e => RequestEvent(e.getEventType.name, e.getBytes, e.getBytesTransferred)
       }
     }
   }
