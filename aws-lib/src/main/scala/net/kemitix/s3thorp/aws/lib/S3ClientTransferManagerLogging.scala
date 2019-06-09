2fd7b89cd416d9d02a39a6e2a99536af6d6448f8
Paul Campbell <pcampbell@kemitix.net>
2019-06-09 18:14:07 +0100

    [aws-lib] display file sizes in english

--- aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientTransferManagerLogging.scala
+++ aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientTransferManagerLogging.scala
@@ -11,8 +11,16 @@ object S3ClientTransferManagerLogging {
                              (implicit info: Int => String => IO[Unit]): IO[Unit] =
     {
       val tryMessage = if (tryCount == 1) "" else s"try $tryCount"
-      val size = localFile.file.length
+      val size = sizeInEnglish(localFile)
       info(1)(s"upload:$tryMessage:$size:${localFile.remoteKey.key}")
+    }
+
+  private def sizeInEnglish(localFile: LocalFile) =
+    localFile.file.length match {
+      case bytes if bytes > 1024 * 1024 * 1024 => s"${bytes / 1024 / 1024 /1024}Gb"
+      case bytes if bytes > 1024 * 1024 => s"${bytes / 1024 / 1024}Mb"
+      case bytes if bytes > 1024 => s"${bytes / 1024}Kb"
+      case bytes => s"${localFile.file.length}b"
     }
 
   def logMultiPartUploadFinished(localFile: LocalFile)
