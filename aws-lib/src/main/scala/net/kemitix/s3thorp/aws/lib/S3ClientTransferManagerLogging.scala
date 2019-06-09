72d5eea17848225bb7d0630f4ea265f45a9a0701
Paul Campbell <pcampbell@kemitix.net>
2019-06-09 12:51:15 +0100

    [aws-lib] remove 'transfer-manager'prefix and only show tryCount > 1

--- aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientTransferManagerLogging.scala
+++ aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientTransferManagerLogging.scala
@@ -6,72 +6,73 @@ import net.kemitix.s3thorp.domain.{LocalFile, MD5Hash}
 
 object S3ClientTransferManagerLogging {
 
-  private val prefix = "transfer-manager"
-
   def logMultiPartUploadStart(localFile: LocalFile,
                               tryCount: Int)
                              (implicit info: Int => String => IO[Unit]): IO[Unit] =
-    info(1)(s"$prefix:upload:try $tryCount: ${localFile.remoteKey.key}")
+    {
+      val tryMessage = if (tryCount == 1) "" else s"try $tryCount"
+      info(1)(s"upload:$tryMessage:${localFile.remoteKey.key}")
+    }
 
   def logMultiPartUploadFinished(localFile: LocalFile)
                                 (implicit info: Int => String => IO[Unit]): IO[Unit] =
-    info(4)(s"$prefix:upload:finished: ${localFile.remoteKey.key}")
+    info(4)(s"upload:finished: ${localFile.remoteKey.key}")
 
   def logMultiPartUploadInitiate(localFile: LocalFile)
                                 (implicit info: Int => String => IO[Unit]): IO[Unit] =
-    info(5)(s"$prefix:initiating: ${localFile.remoteKey.key}")
+    info(5)(s"initiating: ${localFile.remoteKey.key}")
 
   def logMultiPartUploadPartsDetails(localFile: LocalFile,
                                      nParts: Int,
                                      partSize: Long)
                                     (implicit info: Int => String => IO[Unit]): IO[Unit] =
-    info(5)(s"$prefix:parts $nParts:each $partSize: ${localFile.remoteKey.key}")
+    info(5)(s"parts $nParts:each $partSize: ${localFile.remoteKey.key}")
 
   def logMultiPartUploadPartDetails(localFile: LocalFile,
                                     partNumber: Int,
                                     partHash: MD5Hash)
                                    (implicit info: Int => String => IO[Unit]): IO[Unit] =
-    info(5)(s"$prefix:part $partNumber:hash ${partHash.hash}: ${localFile.remoteKey.key}")
+    info(5)(s"part $partNumber:hash ${partHash.hash}: ${localFile.remoteKey.key}")
 
   def logMultiPartUploadPart(localFile: LocalFile,
                              partRequest: UploadPartRequest)
                             (implicit info: Int => String => IO[Unit]): IO[Unit] =
-    info(5)(s"$prefix:sending:part ${partRequest.getPartNumber}: ${partRequest.getMd5Digest}: ${localFile.remoteKey.key}")
+    info(5)(s"sending:part ${partRequest.getPartNumber}: ${partRequest.getMd5Digest}: ${localFile.remoteKey.key}")
 
   def logMultiPartUploadPartDone(localFile: LocalFile,
                                  partRequest: UploadPartRequest,
                                  result: UploadPartResult)
                                 (implicit info: Int => String => IO[Unit]): IO[Unit] =
-    info(5)(s"$prefix:sent:part ${partRequest.getPartNumber}: ${result.getPartETag}: ${localFile.remoteKey.key}")
+    info(5)(s"sent:part ${partRequest.getPartNumber}: ${result.getPartETag}: ${localFile.remoteKey.key}")
 
   def logMultiPartUploadPartError(localFile: LocalFile,
                                   partRequest: UploadPartRequest,
                                   error: AmazonS3Exception)
                                  (implicit warn: String => IO[Unit]): IO[Unit] = {
     val returnedMD5Hash = error.getAdditionalDetails.get("Content-MD5")
-    warn(s"$prefix:error:part ${partRequest.getPartNumber}:ret-hash $returnedMD5Hash: ${localFile.remoteKey.key}")
+    warn(s"error:part ${partRequest.getPartNumber}:ret-hash $returnedMD5Hash: ${localFile.remoteKey.key}")
   }
 
   def logMultiPartUploadCompleted(createUploadResponse: InitiateMultipartUploadResult,
                                   uploadPartResponses: Stream[UploadPartResult],
                                   localFile: LocalFile)
                                  (implicit info: Int => String => IO[Unit]): IO[Unit] =
-    info(1)(s"$prefix:completed:parts ${uploadPartResponses.size}: ${localFile.remoteKey.key}")
+    info(1)(s"completed:parts ${uploadPartResponses.size}: ${localFile.remoteKey.key}")
 
   def logMultiPartUploadCancelling(localFile: LocalFile)
                                   (implicit warn: String => IO[Unit]): IO[Unit] =
-    warn(s"$prefix:cancelling: ${localFile.remoteKey.key}")
+    warn(s"cancelling: ${localFile.remoteKey.key}")
 
   def logErrorRetrying(e: Throwable, localFile: LocalFile, tryCount: Int)
                       (implicit warn: String => IO[Unit]): IO[Unit] =
-    warn(s"$prefix:retry:error ${e.getMessage}: ${localFile.remoteKey.key}")
+    warn(s"retry:error ${e.getMessage}: ${localFile.remoteKey.key}")
 
   def logErrorCancelling(e: Throwable, localFile: LocalFile)
                         (implicit error: String => IO[Unit]) : IO[Unit] =
-    error(s"$prefix:cancelling:error ${e.getMessage}: ${localFile.remoteKey.key}")
+    error(s"cancelling:error ${e.getMessage}: ${localFile.remoteKey.key}")
 
   def logErrorUnknown(e: Throwable, localFile: LocalFile)
                      (implicit error: String => IO[Unit]): IO[Unit] =
-    error(s"$prefix:unknown:error $e: ${localFile.remoteKey.key}")
+    error(s"unknown:error $e: ${localFile.remoteKey.key}")
 
 }
