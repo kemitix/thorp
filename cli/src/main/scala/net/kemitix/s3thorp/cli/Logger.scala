f1b5f5be8bdcfed0969c7691bea430cae471dbb0
Paul Campbell <pcampbell@kemitix.net>
2019-06-09 17:58:59 +0100

    [cli] include correct level in info messages

--- cli/src/main/scala/net/kemitix/s3thorp/cli/Logger.scala
+++ cli/src/main/scala/net/kemitix/s3thorp/cli/Logger.scala
@@ -5,7 +5,7 @@ import cats.effect.IO
 class Logger(verbosity: Int)  {
 
   def info(level: Int)(message: String): IO[Unit] =
-    if (verbosity >= level) IO(println(s"[INFO:1] $message"))
+    if (verbosity >= level) IO(println(s"[INFO:$level] $message"))
     else IO.unit
 
   def warn(message: String): IO[Unit] = IO(println(s"[  WARN] $message"))
