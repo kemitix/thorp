86bae2d4297b813afb29807a1135d15021fa3553
Paul Campbell <pcampbell@kemitix.net>
2019-06-09 18:13:46 +0100

    [aws-lib] S3ClientLogging adjust logging levels

--- aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientLogging.scala
+++ aws-lib/src/main/scala/net/kemitix/s3thorp/aws/lib/S3ClientLogging.scala
@@ -22,36 +22,36 @@ object S3ClientLogging {
                      bucket: Bucket)
                     (implicit info: Int => String => IO[Unit]): PutObjectResult => IO[PutObjectResult] =
     in => for {
-      _ <- info(4)(s"Uploading: ${bucket.name}:${localFile.remoteKey.key}")
+      _ <- info(1)(s"Uploading: ${bucket.name}:${localFile.remoteKey.key}")
     } yield in
 
   def logUploadFinish(localFile: LocalFile,
                       bucket: Bucket)
                      (implicit info: Int => String => IO[Unit]): PutObjectResult => IO[Unit] =
-    _ => info(1)(s"Uploaded: ${bucket.name}:${localFile.remoteKey.key}")
+    _ => info(2)(s"Uploaded: ${bucket.name}:${localFile.remoteKey.key}")
 
   def logCopyStart(bucket: Bucket,
                    sourceKey: RemoteKey,
                    targetKey: RemoteKey)
                   (implicit info: Int => String => IO[Unit]): CopyObjectResult => IO[CopyObjectResult] =
     in => for {
-      _ <- info(4)(s"Copy: ${bucket.name}:${sourceKey.key} => ${targetKey.key}")
+      _ <- info(1)(s"Copy: ${bucket.name}:${sourceKey.key} => ${targetKey.key}")
     } yield in
 
   def logCopyFinish(bucket: Bucket,
                     sourceKey: RemoteKey,
                     targetKey: RemoteKey)
                    (implicit info: Int => String => IO[Unit]): CopyObjectResult => IO[Unit] =
-    _ => info(3)(s"Copied: ${bucket.name}:${sourceKey.key} => ${targetKey.key}")
+    _ => info(2)(s"Copied: ${bucket.name}:${sourceKey.key} => ${targetKey.key}")
 
   def logDeleteStart(bucket: Bucket,
                      remoteKey: RemoteKey)
                     (implicit info: Int => String => IO[Unit]): IO[Unit] =
-      info(4)(s"Delete: ${bucket.name}:${remoteKey.key}")
+      info(1)(s"Delete: ${bucket.name}:${remoteKey.key}")
 
   def logDeleteFinish(bucket: Bucket,
                       remoteKey: RemoteKey)
                      (implicit info: Int => String => IO[Unit]): IO[Unit] =
-      info(3)(s"Deleted: ${bucket.name}:${remoteKey.key}")
+      info(2)(s"Deleted: ${bucket.name}:${remoteKey.key}")
 
 }
