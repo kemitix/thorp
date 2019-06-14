3a23bdef1f4776ee69cf0c9ed618cdd14fef0329
Paul Campbell <pcampbell@kemitix.net>
2019-06-14 08:17:56 +0100

    [core] MD5HashGeneratorTest switch to Id from IO

--- core/src/test/scala/net/kemitix/s3thorp/core/MD5HashGeneratorTest.scala
+++ core/src/test/scala/net/kemitix/s3thorp/core/MD5HashGeneratorTest.scala
@@ -1,8 +1,6 @@
 package net.kemitix.s3thorp.core
 
-import java.nio.file.Files
-
-import cats.effect.IO
+import cats.Id
 import net.kemitix.s3thorp.core.MD5HashData.rootHash
 import net.kemitix.s3thorp.domain.{Bucket, Config, MD5Hash, RemoteKey}
 import org.scalatest.FunSpec
@@ -12,12 +10,12 @@ class MD5HashGeneratorTest extends FunSpec {
   private val source = Resource(this, "upload")
   private val prefix = RemoteKey("prefix")
   implicit private val config: Config = Config(Bucket("bucket"), prefix, source = source)
-  implicit private val logInfo: Int => String => IO[Unit] = l => i => IO.unit
+  implicit private val logInfo: Int => String => Id[Unit] = l => i => ()
 
     describe("read a small file (smaller than buffer)") {
       val file = Resource(this, "upload/root-file")
       it("should generate the correct hash") {
-        val result = MD5HashGenerator.md5File[IO](file).unsafeRunSync
+        val result = MD5HashGenerator.md5File[Id](file)
         assertResult(rootHash)(result)
       }
     }
@@ -25,7 +23,7 @@ class MD5HashGeneratorTest extends FunSpec {
       val file = Resource(this, "big-file")
       it("should generate the correct hash") {
         val expected = MD5Hash("b1ab1f7680138e6db7309200584e35d8")
-        val result = MD5HashGenerator.md5File[IO](file).unsafeRunSync
+        val result = MD5HashGenerator.md5File[Id](file)
         assertResult(expected)(result)
       }
     }
