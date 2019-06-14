82fbd5ce7639bf4fc10db349f16ca1e6a3275d8f
Paul Campbell <pcampbell@kemitix.net>
2019-06-14 12:56:10 +0100

    [cli] Program extracted from Main

--- cli/src/main/scala/net/kemitix/s3thorp/cli/Main.scala
+++ cli/src/main/scala/net/kemitix/s3thorp/cli/Main.scala
@@ -1,13 +1,9 @@
 package net.kemitix.s3thorp.cli
 
-import java.io.File
 import java.nio.file.Paths
 
 import cats.effect.ExitCase.{Canceled, Completed, Error}
 import cats.effect.{ExitCode, IO, IOApp}
-import net.kemitix.s3thorp.core.MD5HashGenerator.md5File
-import net.kemitix.s3thorp.aws.lib.S3ClientBuilder
-import net.kemitix.s3thorp.core.Sync
 import net.kemitix.s3thorp.domain.Config
 
 object Main extends IOApp {
@@ -15,25 +11,10 @@ object Main extends IOApp {
   val defaultConfig: Config =
     Config(source = Paths.get(".").toFile)
 
-  def program(config: Config): IO[ExitCode] = {
-    val logger = new Logger[IO] (config.verbose)
-    val info = (l: Int) => (m: String) => logger.info(l) (m)
-    val warn = (w: String) => logger.warn(w)
-    for {
-      _ <- logger.info(1)("S3Thorp - hashed sync for s3")
-      _ <- Sync.run[IO](config, S3ClientBuilder.defaultClient, hashGenerator(info), info, warn)
-    } yield ExitCode.Success
-  }
-
-  private def hashGenerator(info: Int => String => IO[Unit]) = {
-    implicit val logInfo: Int => String => IO[Unit] = info
-    file: File => md5File[IO](file)
-  }
-
   override def run(args: List[String]): IO[ExitCode] = {
     val logger = new Logger[IO](1)
     ParseArgs(args, defaultConfig)
-      .map(program)
+      .map(Program[IO])
       .getOrElse(IO(ExitCode.Error))
       .guaranteeCase {
           case Canceled => logger.warn("Interrupted")
