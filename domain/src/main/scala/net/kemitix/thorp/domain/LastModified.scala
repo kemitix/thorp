660bdb0b93b1af9f89a2aaf6364f54eba4279335
Paul Campbell <pcampbell@kemitix.net>
2019-06-15 08:34:47 +0100

    [domain] LastModified can match against a epoch millisecond

--- domain/src/main/scala/net/kemitix/thorp/domain/LastModified.scala
+++ domain/src/main/scala/net/kemitix/thorp/domain/LastModified.scala
@@ -1,5 +1,13 @@
 package net.kemitix.thorp.domain
 
 import java.time.Instant
+import java.time.temporal.ChronoField
 
-final case class LastModified(when: Instant)
+final case class LastModified(when: Instant) {
+
+  def matches(epochMilliseconds: Long): Boolean = {
+    val millis = when.getLong(ChronoField.MILLI_OF_SECOND)
+    (when.getEpochSecond * 1000) + millis == epochMilliseconds
+  }
+
+}
---
+++ domain/src/test/scala/net/kemitix/s3thorp/domain/LastModifiedTest.scala
@@ -0,0 +1,32 @@
+package net.kemitix.s3thorp.domain
+
+import java.time.Instant
+import java.time.temporal.ChronoField
+
+import org.scalatest.FunSpec
+
+class LastModifiedTest extends FunSpec {
+
+  describe("match against a file last modified") {
+    val fileLastModified = 1560464353000L
+    describe("when time stamps are identical") {
+      val lastModified = LastModified(Instant.ofEpochMilli(fileLastModified))
+      it("how to get epochMillis from an Instant") {
+        val when = lastModified.when
+        val epochSecond = when.getEpochSecond
+        val millis = when.getLong(ChronoField.MILLI_OF_SECOND)
+        val epochMillis = (epochSecond * 1000) + millis
+        assertResult(fileLastModified)(epochMillis)
+      }
+      it("should match") {
+        assertResult(true)(lastModified.matches(fileLastModified))
+      }
+    }
+    describe("when time stamps are out by 1 millisecond") {
+      val lastModified = LastModified(Instant.ofEpochMilli(fileLastModified + 1))
+      it("should not match") {
+        assertResult(false)(lastModified.matches(fileLastModified))
+      }
+    }
+  }
+}
