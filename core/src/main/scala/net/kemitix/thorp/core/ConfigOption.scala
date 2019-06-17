65dbdf08ef4453f2ced524475da031d255e60874
Paul Campbell <pcampbell@kemitix.net>
2019-06-17 21:42:35 +0100

    [core] ConfigOption can update a Config

--- core/src/main/scala/net/kemitix/thorp/core/ConfigOption.scala
+++ core/src/main/scala/net/kemitix/thorp/core/ConfigOption.scala
@@ -2,13 +2,30 @@ package net.kemitix.thorp.core
 
 import java.nio.file.Path
 
-sealed trait ConfigOption
+import net.kemitix.thorp.domain
+import net.kemitix.thorp.domain.{Config, RemoteKey}
+
+sealed trait ConfigOption {
+  def update(config: Config): Config
+}
 
 object ConfigOption {
-  case class Source(path: Path) extends ConfigOption
-  case class Bucket(name: String) extends ConfigOption
-  case class Prefix(path: String) extends ConfigOption
-  case class Include(pattern: String) extends ConfigOption
-  case class Exclude(pattern: String) extends ConfigOption
-  case class Debug() extends ConfigOption
+  case class Source(path: Path) extends ConfigOption {
+    override def update(config: Config): Config = config.copy(source = path.toFile)
+  }
+  case class Bucket(name: String) extends ConfigOption {
+    override def update(config: Config): Config = config.copy(bucket = domain.Bucket(name))
+  }
+  case class Prefix(path: String) extends ConfigOption {
+    override def update(config: Config): Config = config.copy(prefix = RemoteKey(path))
+  }
+  case class Include(pattern: String) extends ConfigOption {
+    override def update(config: Config): Config = config.copy(filters = domain.Filter.Include(pattern) :: config.filters)
+  }
+  case class Exclude(pattern: String) extends ConfigOption {
+    override def update(config: Config): Config = config.copy(filters = domain.Filter.Exclude(pattern) :: config.filters)
+  }
+  case class Debug() extends ConfigOption {
+    override def update(config: Config): Config = config.copy(debug = true)
+  }
 }
