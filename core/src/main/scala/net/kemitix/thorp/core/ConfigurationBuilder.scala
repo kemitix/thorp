c62ed90219c54b8710c670b73272dbef6b33ea2c
Paul Campbell <pcampbell@kemitix.net>
2019-06-18 14:55:59 +0100

    [core] refactor ConfigurationBuilder

--- core/src/main/scala/net/kemitix/thorp/core/ConfigurationBuilder.scala
+++ core/src/main/scala/net/kemitix/thorp/core/ConfigurationBuilder.scala
@@ -3,8 +3,9 @@ package net.kemitix.thorp.core
 import java.io.File
 import java.nio.file.Paths
 
-import cats.data.{NonEmptyChain, Validated, ValidatedNec}
+import cats.data.NonEmptyChain
 import net.kemitix.thorp.domain.Config
+import net.kemitix.thorp.core.ConfigValidator._
 
 /**
   * Builds a configuration from settings in a file within the
@@ -12,13 +13,15 @@ import net.kemitix.thorp.domain.Config
   */
 trait ConfigurationBuilder {
 
-  val defaultConfig: Config =
-    Config(source = Paths.get(System.getenv("PWD")).toFile)
+  private val pwdFile: File = Paths.get(System.getenv("PWD")).toFile
 
-  def apply(configOptions: Seq[ConfigOption]): Either[NonEmptyChain[ConfigValidation], Config] = {
-    val config = configOptions.foldRight(defaultConfig)((co, c) => co.update(c))
-    ConfigValidator.validateConfig(config).toEither
-  }
+  private val defaultConfig: Config = Config(source = pwdFile)
+
+  def apply(configOptions: Seq[ConfigOption]): Either[NonEmptyChain[ConfigValidation], Config] =
+    validateConfig(buildConfig(configOptions)).toEither
+
+  private def buildConfig(configOptions: Seq[ConfigOption]) =
+    configOptions.foldRight(defaultConfig)((co, c) => co.update(c))
 
 }
 
