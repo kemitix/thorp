74062bf1d7f61da515203ce268721c736f3937a2
Paul Campbell <pcampbell@kemitix.net>
2019-06-19 09:45:32 +0100

    [core] Add ParseConfigFile

---
+++ core/src/main/scala/net/kemitix/thorp/core/ParseConfigFile.scala
@@ -0,0 +1,48 @@
+package net.kemitix.thorp.core
+
+import java.nio.file.{Files, Path, Paths}
+import java.util.regex.Pattern
+
+import cats.effect.IO
+import net.kemitix.thorp.core.ConfigOption.{Bucket, Debug, Exclude, Include, Prefix, Source}
+
+import scala.collection.JavaConverters._
+
+trait ParseConfigFile {
+
+  def apply(filename: Path): IO[Seq[ConfigOption]] =
+    readFile(filename).map(parseLines)
+
+  private def readFile(filename: Path) =
+    IO {
+      val lines = Files.lines(filename)
+      val scala = lines.iterator.asScala.toList
+      lines.close()
+      scala
+    }
+
+  private def parseLines(lines: List[String]) =
+    lines.flatMap(parseLine)
+
+  private val format = Pattern.compile("^(<?:key>)\\s*=\\s*(<?:value>)\\s*$")
+
+  private def parseLine(str: String) =
+    format.matcher(str) match {
+      case m if m.matches => parse(m.group("key"), m.group("value"))
+      case _ => None
+    }
+
+  private def parse(key: String, value: String): Option[ConfigOption] =
+    key match {
+      case "source" => Some(Source(Paths.get(value)))
+      case "bucket" => Some(Bucket(value))
+      case "prefix" => Some(Prefix(value))
+      case "include" => Some(Include(value))
+      case "exclude" => Some(Exclude(value))
+      case "debug" => Some(Debug())
+      case _ => None
+    }
+
+}
+
+object ParseConfigFile extends ParseConfigFile
