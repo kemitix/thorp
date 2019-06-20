cb2ac23680394d4ee44a7f1e86a83fc55d4047a1
Paul Campbell <pcampbell@kemitix.net>
2019-06-20 17:13:08 +0100

    [core] ConfigurationBuilder reads user and global config files

--- core/src/main/scala/net/kemitix/thorp/core/ConfigurationBuilder.scala
+++ core/src/main/scala/net/kemitix/thorp/core/ConfigurationBuilder.scala
@@ -6,8 +6,8 @@ import java.nio.file.Paths
 import cats.data.NonEmptyChain
 import cats.effect.IO
 import net.kemitix.thorp.core.ConfigValidator.validateConfig
-import net.kemitix.thorp.domain.Config
 import net.kemitix.thorp.core.ParseConfigFile.parseFile
+import net.kemitix.thorp.domain.Config
 
 /**
   * Builds a configuration from settings in a file within the
@@ -22,8 +22,10 @@ trait ConfigurationBuilder {
   def buildConfig(priorityOptions: Seq[ConfigOption]): IO[Either[NonEmptyChain[ConfigValidation], Config]] = {
     val source = findSource(priorityOptions)
     for {
-      options <- sourceOptions(source)
-      collected = priorityOptions ++ options
+      sourceOptions <- sourceOptions(source)
+      userOptions <- userOptions()
+      globalOptions <- globalOptions()
+      collected = priorityOptions ++ sourceOptions ++ userOptions ++ globalOptions
       config = collateOptions(collected)
     } yield validateConfig(config).toEither
   }
@@ -36,6 +38,14 @@ trait ConfigurationBuilder {
 
   private def sourceOptions(source: File): IO[Seq[ConfigOption]] =
     readFile(source, ".thorp.conf")
+
+  private def userOptions(): IO[Seq[ConfigOption]] =
+    readFile(userHome, ".config/thorp.conf")
+
+  private def globalOptions(): IO[Seq[ConfigOption]] =
+    parseFile(Paths.get("/etc/thorp.conf"))
+
+  private def userHome = new File(System.getProperty("user.home"))
 
   private def readFile(source: File, filename: String): IO[Seq[ConfigOption]] =
     parseFile(source.toPath.resolve(filename))
