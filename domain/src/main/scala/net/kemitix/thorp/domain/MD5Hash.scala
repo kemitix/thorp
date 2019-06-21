e79b3ee2bcd6bad57603b896ad4419eb4dca5d6c
Paul Campbell <pcampbell@kemitix.net>
2019-06-21 19:08:45 +0100

    [domain] MD5Hash now can optionally store the base64 encoded hash

--- domain/src/main/scala/net/kemitix/thorp/domain/MD5Hash.scala
+++ domain/src/main/scala/net/kemitix/thorp/domain/MD5Hash.scala
@@ -1,9 +1,16 @@
 package net.kemitix.thorp.domain
 
+import java.util.Base64
+
 import net.kemitix.thorp.domain.QuoteStripper.stripQuotes
 
-final case class MD5Hash(in: String) {
+final case class MD5Hash(in: String, hash64: Option[String] = None) {
 
   lazy val hash: String = in filter stripQuotes
 
+}
+
+object MD5Hash {
+  def fromDigest(digest: Array[Byte]): MD5Hash =
+    MD5Hash((digest map ("%02x" format _)).mkString, Some(Base64.getEncoder.encodeToString(digest)))
 }
