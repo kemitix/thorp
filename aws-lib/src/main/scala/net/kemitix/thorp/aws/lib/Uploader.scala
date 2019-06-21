6d74c4afc139f0b6d1d40bde3cd615fe0a358a2a
Paul Campbell <pcampbell@kemitix.net>
2019-06-21 19:10:25 +0100

    [aws-lib] Uploader use the base64 encoded hash

--- aws-lib/src/main/scala/net/kemitix/thorp/aws/lib/Uploader.scala
+++ aws-lib/src/main/scala/net/kemitix/thorp/aws/lib/Uploader.scala
@@ -45,7 +45,7 @@ class Uploader(transferManager: => AmazonTransferManager) {
 
   private def request(localFile: LocalFile, bucket: Bucket, listener: ProgressListener): PutObjectRequest = {
     val metadata = new ObjectMetadata()
-    metadata.setContentMD5(localFile.hash.hash)
+    localFile.hash.hash64.foreach(metadata.setContentMD5)
     new PutObjectRequest(bucket.name, localFile.remoteKey.key, localFile.file)
       .withMetadata(metadata)
       .withGeneralProgressListener(listener)
