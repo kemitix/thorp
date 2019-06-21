627fc9b12738d7e9a54ce5f483793a1603cb6807
Paul Campbell <pcampbell@kemitix.net>
2019-06-21 18:13:11 +0100

    [core] SyncSuite invoke subject inside it method and after declaring expectations

--- core/src/test/scala/net/kemitix/thorp/core/SyncSuite.scala
+++ core/src/test/scala/net/kemitix/thorp/core/SyncSuite.scala
@@ -41,20 +41,21 @@ class SyncSuite
       val s3Client = new RecordingClient(testBucket, S3ObjectsData(
         byHash = Map(),
         byKey = Map()))
-      invokeSubject(s3Client, configOptions)
       it("uploads all files") {
         val expectedUploads = Map(
           "subdir/leaf-file" -> leafRemoteKey,
-          "root-file" -> rootRemoteKey
-        )
+          "root-file" -> rootRemoteKey)
+        invokeSubject(s3Client, configOptions)
         assertResult(expectedUploads)(s3Client.uploadsRecord)
       }
       it("copies nothing") {
         val expectedCopies = Map()
+        invokeSubject(s3Client, configOptions)
         assertResult(expectedCopies)(s3Client.copiesRecord)
       }
       it("deletes nothing") {
         val expectedDeletions = Set()
+        invokeSubject(s3Client, configOptions)
         assertResult(expectedDeletions)(s3Client.deletionsRecord)
       }
     }
@@ -67,17 +68,19 @@ class SyncSuite
           RemoteKey("prefix/root-file") -> HashModified(rootHash, lastModified),
           RemoteKey("prefix/subdir/leaf-file") -> HashModified(leafHash, lastModified)))
       val s3Client = new RecordingClient(testBucket, s3ObjectsData)
-      invokeSubject(s3Client, configOptions)
       it("uploads nothing") {
         val expectedUploads = Map()
+        invokeSubject(s3Client, configOptions)
         assertResult(expectedUploads)(s3Client.uploadsRecord)
       }
       it("copies nothing") {
         val expectedCopies = Map()
+        invokeSubject(s3Client, configOptions)
         assertResult(expectedCopies)(s3Client.copiesRecord)
       }
       it("deletes nothing") {
         val expectedDeletions = Set()
+        invokeSubject(s3Client, configOptions)
         assertResult(expectedDeletions)(s3Client.deletionsRecord)
       }
     }
@@ -91,17 +94,19 @@ class SyncSuite
           RemoteKey("prefix/root-file-old") -> HashModified(rootHash, lastModified),
           RemoteKey("prefix/subdir/leaf-file") -> HashModified(leafHash, lastModified)))
       val s3Client = new RecordingClient(testBucket, s3ObjectsData)
-      invokeSubject(s3Client, configOptions)
       it("uploads nothing") {
+        invokeSubject(s3Client, configOptions)
         val expectedUploads = Map()
         assertResult(expectedUploads)(s3Client.uploadsRecord)
       }
       it("copies the file") {
         val expectedCopies = Map(RemoteKey("prefix/root-file-old") -> RemoteKey("prefix/root-file"))
+        invokeSubject(s3Client, configOptions)
         assertResult(expectedCopies)(s3Client.copiesRecord)
       }
       it("deletes the original") {
         val expectedDeletions = Set(RemoteKey("prefix/root-file-old"))
+        invokeSubject(s3Client, configOptions)
         assertResult(expectedDeletions)(s3Client.deletionsRecord)
       }
     }
@@ -119,20 +124,20 @@ class SyncSuite
         byKey = Map(
           deletedKey -> HashModified(deletedHash, lastModified)))
       val s3Client = new RecordingClient(testBucket, s3ObjectsData)
-      invokeSubject(s3Client, configOptions)
       it("deleted key") {
         val expectedDeletions = Set(deletedKey)
+        invokeSubject(s3Client, configOptions)
         assertResult(expectedDeletions)(s3Client.deletionsRecord)
       }
     }
     describe("when a file is excluded") {
       val s3ObjectsData = S3ObjectsData(Map(), Map())
       val s3Client = new RecordingClient(testBucket, s3ObjectsData)
-      invokeSubject(s3Client, ConfigOption.Exclude("leaf") :: configOptions)
       it("is not uploaded") {
         val expectedUploads = Map(
           "root-file" -> rootRemoteKey
         )
+        invokeSubject(s3Client, ConfigOption.Exclude("leaf") :: configOptions)
         assertResult(expectedUploads)(s3Client.uploadsRecord)
       }
     }
