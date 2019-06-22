890ce8cf6127e9c61aed14c44cda8f7528cdc8ee
Paul Campbell <pcampbell@kemitix.net>
2019-06-22 23:18:22 +0100

    [core] Synchronise rewritten

--- core/src/main/scala/net/kemitix/thorp/core/Synchronise.scala
+++ core/src/main/scala/net/kemitix/thorp/core/Synchronise.scala
@@ -16,59 +16,49 @@ trait Synchronise {
 
   def useValidConfig(storageService: StorageService,
                      logger: Logger,
-                     config: Config): IO[Either[List[String], Stream[Action]]] = {
-    implicit val l: Logger = logger
-    implicit val c: Config = config
+                     config: Config): IO[Either[List[String], Stream[Action]]] =
+    gatherMetadata(storageService, logger, config)
+      .map { md => 
+        val (rd, ld) = md
+        val actions1 = actionsForLocalFiles(config, ld)
+        val actions2 = actionsForRemoteKeys(config, rd)
+        Right(actions1 ++ actions2)
+      }
 
+  private def gatherMetadata(storageService: StorageService,
+                             logger: Logger,
+                             config: Config) =
     for {
-      complete <- for {
-        scanRemoteKeys <- for {
-          scanLocalFiles <- for {
-            remoteData <- fetchRemoteData(storageService, logger, config)
-            localFiles <- findLocalFiles
-          } yield gatherMetadata(config, remoteData, localFiles)
-        } yield actionsForLocalFiles(config, scanLocalFiles)
-      } yield allActions(config, scanRemoteKeys)
-    } yield Right(complete.actions)
-  }
+      remoteData <- fetchRemoteData(storageService, logger, config)
+      localData <- findLocalFiles(config, logger)
+    } yield (remoteData, localData)
 
-  private def allActions(config: Config, scanRemoteKeys: AppState.ScanRemoteKeys) = {
-    scanRemoteKeys.toCompleted(scanRemoteKeys.remoteData.foldLeft(scanRemoteKeys.actionsFromLocalFiles)((acc, rk) => createActionFromRemoteKey(config, rk) #:: acc))
-  }
+  private def actionsForRemoteKeys(config: Config, remoteData: S3ObjectsData) =
+    remoteData.byKey.keys.foldLeft(Stream[Action]())((acc, rk) => createActionFromRemoteKey(config, rk) #:: acc)
 
-  private def actionsForLocalFiles(config: Config, scanLocalFiles: AppState.ScanLocalFiles) = {
-    scanLocalFiles.toScanRemoteKeys(scanLocalFiles.localData.foldLeft(Stream[Action]())((acc, lf) => createActionFromLocalFile(config, lf) #:: acc))
-  }
+  private def actionsForLocalFiles(config: Config, localData: Stream[LocalFile]) =
+    localData.foldLeft(Stream[Action]())((acc, lf) => createActionFromLocalFile(config, lf) #:: acc)
 
-  private def gatherMetadata(config: Config, remoteData: S3ObjectsData, localFiles: Stream[LocalFile]) = {
-    AppState.Initial().toConfigured(config).toScanLocalFiles(remoteData, localFiles)
-  }
-
-  private def findLocalFiles(implicit config: Config, l: Logger) = {
+  private def findLocalFiles(implicit config: Config, l: Logger) =
     LocalFileStream.findFiles(config.source, MD5HashGenerator.md5File(_))
-  }
 
-  private def fetchRemoteData(storageService: StorageService, logger: Logger, config: Config) = {
+  private def fetchRemoteData(storageService: StorageService, logger: Logger, config: Config) =
     storageService.listObjects(config.bucket, config.prefix)(logger)
-  }
 
-  private def createActionFromRemoteKey(c: Config, rk: RemoteKey) = {
+  private def createActionFromRemoteKey(c: Config, rk: RemoteKey) =
     DoNothing(c.bucket, rk)
-  }
 
-  private def createActionFromLocalFile(c: Config, lf: LocalFile) = {
+  private def createActionFromLocalFile(c: Config, lf: LocalFile) =
     DoNothing(c.bucket, lf.remoteKey)
-  }
 
   def apply(storageService: StorageService,
             configOptions: Seq[ConfigOption])
-           (implicit logger: Logger): IO[Either[List[String], Stream[Action]]] = {
+           (implicit logger: Logger): IO[Either[List[String], Stream[Action]]] =
     ConfigurationBuilder.buildConfig(configOptions)
       .flatMap {
         case Right(config) => useValidConfig(storageService, logger, config)
         case Left(errors) => IO.pure(Left(errorMessages(errors.toList)))
       }
-  }
 
 }
 
