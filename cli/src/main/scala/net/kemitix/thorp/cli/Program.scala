ec942ff3a205641bd2d2203fb7b19bff5d5cacec
Paul Campbell <pcampbell@kemitix.net>
2019-06-23 16:58:11 +0100

    [cli] Program rewritten to use Synchronise

--- cli/src/main/scala/net/kemitix/thorp/cli/Program.scala
+++ cli/src/main/scala/net/kemitix/thorp/cli/Program.scala
@@ -1,24 +1,34 @@
 package net.kemitix.thorp.cli
 
 import cats.effect.{ExitCode, IO}
-import net.kemitix.thorp.core.{ConfigOption, Sync}
-import net.kemitix.thorp.domain.Logger
+import cats.implicits._
+import net.kemitix.thorp.core.{Action, ActionSubmitter, ConfigOption, Synchronise}
+import net.kemitix.thorp.domain.{Logger, StorageQueueEvent}
 import net.kemitix.thorp.storage.aws.S3StorageServiceBuilder.defaultStorageService
 
 trait Program {
 
   def apply(cliOptions: Seq[ConfigOption]): IO[ExitCode] = {
     implicit val logger: Logger = new PrintLogger()
-    Sync(defaultStorageService)(cliOptions) flatMap {
+    Synchronise(defaultStorageService, cliOptions).flatMap {
       case Left(errors) =>
         for {
           _ <- logger.error(s"There were errors:")
-          _ <- IO.pure(errors.map(error => logger.error(s" - $error")))
+          _ <- errors.map(error => logger.error(s" - $error")).sequence
         } yield ExitCode.Error
-      case Right(_) => IO.pure(ExitCode.Success)
+      case Right(actions) =>
+        for {
+          _ <- handleActions(actions)
+        } yield ExitCode.Success
     }
   }
 
+  private def handleActions(actions: Stream[Action])
+                           (implicit l: Logger): IO[Stream[StorageQueueEvent]] = {
+    actions.foldRight(Stream[IO[StorageQueueEvent]]()) {
+      (action, stream) => ActionSubmitter.submitAction(defaultStorageService, action) ++ stream
+    }.sequence
+  }
 }
 
 object Program extends Program
