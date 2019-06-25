877ca48309085584762be617887914248f19e94e
Paul Campbell <pcampbell@kemitix.net>
2019-06-25 07:36:29 +0100

    [core] MD4HashData add missing base64 digest for leafFile

--- core/src/test/scala/net/kemitix/thorp/core/MD5HashData.scala
+++ core/src/test/scala/net/kemitix/thorp/core/MD5HashData.scala
@@ -6,6 +6,6 @@ object MD5HashData {
 
   val rootHash = MD5Hash("a3a6ac11a0eb577b81b3bb5c95cc8a6e", Some("o6asEaDrV3uBs7tclcyKbg=="))
 
-  val leafHash = MD5Hash("208386a650bdec61cfcd7bd8dcb6b542")
+  val leafHash = MD5Hash("208386a650bdec61cfcd7bd8dcb6b542", Some("IIOGplC97GHPzXvY3La1Qg=="))
 
 }
