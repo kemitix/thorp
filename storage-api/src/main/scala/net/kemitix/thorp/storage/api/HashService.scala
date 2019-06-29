cd64e8a68f0fad1f45aebb623f8e03d82d3e5094
Paul Campbell <pcampbell@kemitix.net>
2019-06-29 07:42:38 +0100

    [storage-api] HashService added

---
+++ storage-api/src/main/scala/net/kemitix/thorp/storage/api/HashService.scala
@@ -0,0 +1,15 @@
+package net.kemitix.thorp.storage.api
+
+import java.io.File
+
+import cats.effect.IO
+import net.kemitix.thorp.domain.{Logger, MD5Hash}
+
+/**
+  * Creates one, or more, hashes for local objects.
+  */
+trait HashService {
+
+  def hashLocalObject(file: File)(implicit l: Logger): IO[Set[MD5Hash]]
+
+}
