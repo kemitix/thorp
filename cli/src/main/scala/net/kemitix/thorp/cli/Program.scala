d8260b0a499b8b9a3c3182c6b68d1195cf6a9e34
Paul Campbell <pcampbell@kemitix.net>
2019-07-02 22:59:47 +0100

    [cli] Program rename actions as syncPlan

--- cli/src/main/scala/net/kemitix/thorp/cli/Program.scala
+++ cli/src/main/scala/net/kemitix/thorp/cli/Program.scala
@@ -16,8 +16,8 @@ trait Program {
         ExitCode.Success
     } else {
       for {
-        actions <- Synchronise.createPlan(defaultStorageService, defaultHashService, cliOptions).valueOrF(handleErrors)
-        events <- handleActions(UnversionedMirrorArchive.default(defaultStorageService, ConfigQuery.batchMode(cliOptions)), actions)
+        syncPlan <- Synchronise.createPlan(defaultStorageService, defaultHashService, cliOptions).valueOrF(handleErrors)
+        events <- handleActions(UnversionedMirrorArchive.default(defaultStorageService, ConfigQuery.batchMode(cliOptions)), syncPlan)
         _ <- defaultStorageService.shutdown
         _ <- SyncLogging.logRunFinished(events)
       } yield ExitCode.Success
