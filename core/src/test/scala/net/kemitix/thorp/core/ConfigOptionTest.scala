178a3b8657a5201a3c835ac37f8b8ddb1d993746
Paul Campbell <pcampbell@kemitix.net>
2019-07-09 20:47:39 +0100

    [core] ConfigOptionTest use TemporaryFolder

--- core/src/test/scala/net/kemitix/thorp/core/ConfigOptionTest.scala
+++ core/src/test/scala/net/kemitix/thorp/core/ConfigOptionTest.scala
@@ -1,32 +1,34 @@
 package net.kemitix.thorp.core
 
-import java.nio.file.Paths
-
 import net.kemitix.thorp.domain.Sources
 import org.scalatest.FunSpec
 
-class ConfigOptionTest extends FunSpec {
+class ConfigOptionTest extends FunSpec with TemporaryFolder {
 
   private val source = Resource(this, "")
   private val sourcePath = source.toPath
 
   describe("when more than one source") {
-    val path1 = sourcePath.resolve("path1")
-    val path2 = sourcePath.resolve("path2")
-    val configOptions = ConfigOptions(List(
-      ConfigOption.Source(path1),
-      ConfigOption.Source(path2),
-      ConfigOption.Bucket("bucket"),
-      ConfigOption.IgnoreGlobalOptions,
-      ConfigOption.IgnoreUserOptions
-    ))
-    val args = ConfigurationBuilder.buildConfig(configOptions).unsafeRunSync
-    it("should successfully parse") {
-      assert(args.isRight, args)
-    }
     it("should preserve their order") {
-      val expected = Sources(List(path1, path2))
-      assertResult(expected)(ConfigQuery.sources(configOptions))
+      withDirectory(path1 => {
+        withDirectory(path2 => {
+          val configOptions = ConfigOptions(List(
+            ConfigOption.Source(path1),
+            ConfigOption.Source(path2),
+            ConfigOption.Bucket("bucket"),
+            ConfigOption.IgnoreGlobalOptions,
+            ConfigOption.IgnoreUserOptions
+          ))
+          val expected = Sources(List(path1, path2))
+          val result = invoke(configOptions)
+          assert(result.isRight, result)
+          assertResult(expected)(ConfigQuery.sources(configOptions))
+        })
+      })
     }
+  }
+
+  private def invoke(configOptions: ConfigOptions) = {
+    ConfigurationBuilder.buildConfig(configOptions).unsafeRunSync
   }
 }
