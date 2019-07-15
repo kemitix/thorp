eccf5e65ac885bec28d2eda035a765487bde4627
Paul Campbell <pcampbell@kemitix.net>
2019-07-15 20:22:46 +0100

    [domain] MD5Hash tweak
    Without doing this we have a file that we don't create a valid md5
    hash for. See #103

--- domain/src/test/scala/net/kemitix/thorp/domain/MD5HashData.scala
+++ domain/src/test/scala/net/kemitix/thorp/domain/MD5HashData.scala
@@ -6,7 +6,6 @@ object MD5HashData {
     val hash   = MD5Hash("a3a6ac11a0eb577b81b3bb5c95cc8a6e")
     val base64 = "o6asEaDrV3uBs7tclcyKbg=="
   }
-
   object Leaf {
     val hash   = MD5Hash("208386a650bdec61cfcd7bd8dcb6b542")
     val base64 = "IIOGplC97GHPzXvY3La1Qg=="
