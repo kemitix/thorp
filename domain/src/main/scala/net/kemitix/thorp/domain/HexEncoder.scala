3f67fab68b181d7e5e5e5b8061568fb6eb391ae7
Paul Campbell <pcampbell@kemitix.net>
2019-07-19 20:47:37 +0100

    [domain] HexEncoder encode to uppercase

--- domain/src/main/scala/net/kemitix/thorp/domain/HexEncoder.scala
+++ domain/src/main/scala/net/kemitix/thorp/domain/HexEncoder.scala
@@ -5,7 +5,9 @@ import java.math.BigInteger
 trait HexEncoder {
 
   def encode(bytes: Array[Byte]): String =
-    String.format("%0" + (bytes.length << 1) + "x", new BigInteger(1, bytes))
+    String
+      .format("%0" + (bytes.length << 1) + "x", new BigInteger(1, bytes))
+      .toUpperCase
 
   def decode(hexString: String): Array[Byte] =
     hexString
