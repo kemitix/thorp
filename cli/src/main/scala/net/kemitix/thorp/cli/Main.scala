38f362bca821ff95b91fe042317dc932b0c62468
Paul Campbell <pcampbell@kemitix.net>
2019-07-24 19:22:02 +0100

    [cli] Main use ZIO#provide to run program

--- cli/src/main/scala/net/kemitix/thorp/cli/Main.scala
+++ cli/src/main/scala/net/kemitix/thorp/cli/Main.scala
@@ -1,17 +1,14 @@
 package net.kemitix.thorp.cli
 
 import net.kemitix.thorp.console.Console
-import zio.internal.PlatformLive
-import zio.{App, Runtime, UIO, ZIO}
+import zio.{App, ZIO}
 
 object Main extends App {
 
-  private val runtime = Runtime(Console.Live, PlatformLive.Default)
-
   override def run(args: List[String]): ZIO[Environment, Nothing, Int] =
-    runtime.unsafeRun {
-      appLogic(args).fold(_ => UIO(1), _ => UIO(0))
-    }
+    appLogic(args)
+      .provide(Console.Live)
+      .fold(_ => 1, _ => 0)
 
   private def appLogic(args: List[String]): ZIO[Console, Throwable, Unit] =
     for {
