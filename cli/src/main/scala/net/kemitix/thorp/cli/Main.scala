7891fea1504a4d906ad7cb8090f9c4549412957a
Paul Campbell <pcampbell@kemitix.net>
2019-07-24 19:27:47 +0100

    [cli] Main define Program type alias

--- cli/src/main/scala/net/kemitix/thorp/cli/Main.scala
+++ cli/src/main/scala/net/kemitix/thorp/cli/Main.scala
@@ -5,12 +5,14 @@ import zio.{App, ZIO}
 
 object Main extends App {
 
+  type Program[A] = ZIO[Console, Throwable, Unit]
+
   override def run(args: List[String]): ZIO[Environment, Nothing, Int] =
     appLogic(args)
       .provide(Console.Live)
       .fold(_ => 1, _ => 0)
 
-  private def appLogic(args: List[String]): ZIO[Console, Throwable, Unit] =
+  private def appLogic(args: List[String]): Program[Unit] =
     for {
       cliOptions <- ParseArgs(args)
       _          <- Program.run(cliOptions)
