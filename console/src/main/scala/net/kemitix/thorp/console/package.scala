d324d52997933e8b5c590bb44235f1f981c81f85
Paul Campbell <pcampbell@kemitix.net>
2019-07-24 19:17:57 +0100

    [console] clean up helpers

--- console/src/main/scala/net/kemitix/thorp/console/package.scala
+++ console/src/main/scala/net/kemitix/thorp/console/package.scala
@@ -2,7 +2,7 @@ package net.kemitix.thorp
 
 import zio.ZIO
 
-package object console extends Console.Service {
+package object console {
 
   final val consoleService: ZIO[Console, Nothing, Console.Service] =
     ZIO.access(_.console)
@@ -10,7 +10,7 @@ package object console extends Console.Service {
   final def putStrLn(line: String): ZIO[Console, Nothing, Unit] =
     ZIO.accessM(_.console putStrLn line)
 
-  override def putStrLn(line: ConsoleOut): ZIO[Console, Nothing, Unit] =
-    putStrLn(line.en)
+  final def putStrLn(line: ConsoleOut): ZIO[Console, Nothing, Unit] =
+    ZIO.accessM(_.console putStrLn line)
 
 }
