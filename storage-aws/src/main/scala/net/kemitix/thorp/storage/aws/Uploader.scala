3921ceabc55018bd37b4eb55f55615ee2f92d1fd
Paul Campbell <pcampbell@kemitix.net>
2019-07-26 22:12:53 +0100

    [storage-aws] Uploader refactoring

--- storage-aws/src/main/scala/net/kemitix/thorp/storage/aws/Uploader.scala
+++ storage-aws/src/main/scala/net/kemitix/thorp/storage/aws/Uploader.scala
@@ -25,33 +25,28 @@ trait Uploader {
       tryCount: Int
   ): UIO[StorageQueueEvent] =
     transfer(transferManager)(localFile, bucket, batchMode, uploadEventListener)
-      .catchAll(
-        e =>
-          UIO.succeed(
-            ErrorQueueEvent(Action.Upload(localFile.remoteKey.key),
-                            localFile.remoteKey,
-                            e)))
+      .catchAll(handleError(localFile.remoteKey))
+
+  private def handleError(
+      remoteKey: RemoteKey): Throwable => UIO[ErrorQueueEvent] = { e =>
+    UIO.succeed(ErrorQueueEvent(Action.Upload(remoteKey.key), remoteKey, e))
+  }
 
   private def transfer(transferManager: => AmazonTransferManager)(
       localFile: LocalFile,
       bucket: Bucket,
       batchMode: Boolean,
       uploadEventListener: UploadEventListener
-  ): Task[StorageQueueEvent] = {
-    val listener: ProgressListener = progressListener(uploadEventListener)
-    val putObjectRequest           = request(localFile, bucket, batchMode, listener)
+  ): Task[StorageQueueEvent] =
     transferManager
-      .upload(putObjectRequest)
+      .upload(
+        request(localFile,
+                bucket,
+                batchMode,
+                progressListener(uploadEventListener)))
       .map(_.waitForUploadResult)
       .map(upload =>
         UploadQueueEvent(RemoteKey(upload.getKey), MD5Hash(upload.getETag)))
-      .catchAll(
-        e =>
-          Task.succeed(
-            ErrorQueueEvent(Action.Upload(localFile.remoteKey.key),
-                            localFile.remoteKey,
-                            e)))
-  }
 
   private def request(
       localFile: LocalFile,
@@ -59,37 +54,44 @@ trait Uploader {
       batchMode: Boolean,
       listener: ProgressListener
   ): PutObjectRequest = {
-    val metadata = new ObjectMetadata()
-    localFile.md5base64.foreach(metadata.setContentMD5)
     val request =
       new PutObjectRequest(bucket.name, localFile.remoteKey.key, localFile.file)
-        .withMetadata(metadata)
+        .withMetadata(metadata(localFile))
     if (batchMode) request
     else request.withGeneralProgressListener(listener)
   }
 
-  private def progressListener(uploadEventListener: UploadEventListener) =
-    new ProgressListener {
-      override def progressChanged(progressEvent: ProgressEvent): Unit =
-        uploadEventListener.listener(eventHandler(progressEvent))
+  private def metadata: LocalFile => ObjectMetadata = localFile => {
+    val metadata = new ObjectMetadata()
+    localFile.md5base64.foreach(metadata.setContentMD5)
+    metadata
+  }
 
-      private def eventHandler(progressEvent: ProgressEvent) = {
-        progressEvent match {
-          case e: ProgressEvent if isTransfer(e) =>
-            TransferEvent(e.getEventType.name)
-          case e: ProgressEvent if isByteTransfer(e) =>
-            ByteTransferEvent(e.getEventType.name)
-          case e: ProgressEvent =>
-            RequestEvent(e.getEventType.name, e.getBytes, e.getBytesTransferred)
-        }
-      }
+  private def progressListener: UploadEventListener => ProgressListener =
+    uploadEventListener =>
+      new ProgressListener {
+        override def progressChanged(progressEvent: ProgressEvent): Unit =
+          uploadEventListener.listener(eventHandler(progressEvent))
+
+        private def eventHandler: ProgressEvent => UploadEvent =
+          progressEvent => {
+            def isTransfer: ProgressEvent => Boolean =
+              _.getEventType.isTransferEvent
+            def isByteTransfer: ProgressEvent => Boolean =
+              _.getEventType.equals(
+                ProgressEventType.RESPONSE_BYTE_TRANSFER_EVENT)
+            progressEvent match {
+              case e: ProgressEvent if isTransfer(e) =>
+                TransferEvent(e.getEventType.name)
+              case e: ProgressEvent if isByteTransfer(e) =>
+                ByteTransferEvent(e.getEventType.name)
+              case e: ProgressEvent =>
+                RequestEvent(e.getEventType.name,
+                             e.getBytes,
+                             e.getBytesTransferred)
+            }
+          }
     }
-
-  private def isTransfer(e: ProgressEvent) =
-    e.getEventType.isTransferEvent
-
-  private def isByteTransfer(e: ProgressEvent) =
-    e.getEventType equals ProgressEventType.RESPONSE_BYTE_TRANSFER_EVENT
 
 }
 
