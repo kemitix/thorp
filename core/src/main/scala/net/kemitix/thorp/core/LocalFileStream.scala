2b088f303962d610aa97a59727492ff1b5a0ee49
Paul Campbell <pcampbell@kemitix.net>
2019-07-28 21:30:43 +0100

    [core] LocalFileStream Refactoring

--- core/src/main/scala/net/kemitix/thorp/core/LocalFileStream.scala
+++ core/src/main/scala/net/kemitix/thorp/core/LocalFileStream.scala
@@ -14,45 +14,18 @@ object LocalFileStream {
       source: Path
   ): TaskR[Config, LocalFiles] = {
 
-    val isIncluded: Path => TaskR[Config, Boolean] =
-      path =>
-        for {
-          filters <- getFilters
-        } yield Filter.isIncluded(filters)(path)
+    def recurseIntoSubDirectories(path: Path): TaskR[Config, LocalFiles] =
+      path.toFile match {
+        case f if f.isDirectory => loop(path)
+        case _                  => pathToLocalFile(hashService)(path)
+      }
 
-    val pathToLocalFile: Path => TaskR[Config, LocalFiles] =
-      path => localFile(hashService)(path)
+    def recurse(paths: Stream[Path]): TaskR[Config, LocalFiles] =
+      for {
+        recursed <- ZIO.foreach(paths)(path => recurseIntoSubDirectories(path))
+      } yield LocalFiles.reduce(recursed.toStream)
 
     def loop(path: Path): TaskR[Config, LocalFiles] = {
-
-      def dirPaths(path: Path): TaskR[Config, Stream[Path]] =
-        for {
-          paths    <- listFiles(path)
-          filtered <- includedDirPaths(paths)
-        } yield filtered
-
-      def includedDirPaths: Stream[Path] => TaskR[Config, Stream[Path]] =
-        paths => {
-          for {
-            flaggedPaths <- TaskR.foreach(paths)(path =>
-              isIncluded(path).map((path, _)))
-          } yield
-            flaggedPaths.toStream
-              .filter({ case (_, included) => included })
-              .map({ case (path, _) => path })
-        }
-
-      def recurseIntoSubDirectories(path: Path): TaskR[Config, LocalFiles] =
-        path.toFile match {
-          case f if f.isDirectory => loop(path)
-          case _                  => pathToLocalFile(path)
-        }
-
-      def recurse(paths: Stream[Path]): TaskR[Config, LocalFiles] =
-        for {
-          recursed <- ZIO.foreach(paths)(path =>
-            recurseIntoSubDirectories(path))
-        } yield LocalFiles.reduce(recursed.toStream)
 
       for {
         paths      <- dirPaths(path)
@@ -63,30 +36,50 @@ object LocalFileStream {
     loop(source)
   }
 
-  def localFile(
-      hashService: HashService
-  ): Path => TaskR[Config, LocalFiles] =
-    path => {
-      val file = path.toFile
-      for {
-        sources <- getSources
-        prefix  <- getPrefix
-        hash    <- hashService.hashLocalObject(path)
-        localFile = LocalFile(file,
-                              sources.forPath(path).toFile,
-                              hash,
-                              generateKey(sources, prefix)(path))
-      } yield
-        LocalFiles(localFiles = Stream(localFile),
-                   count = 1,
-                   totalSizeBytes = file.length)
-    }
+  private def dirPaths(path: Path) =
+    for {
+      paths    <- listFiles(path)
+      filtered <- includedDirPaths(paths)
+    } yield filtered
 
-  private def listFiles(path: Path): Task[Stream[Path]] =
+  private def includedDirPaths(paths: Stream[Path]) =
+    for {
+      flaggedPaths <- TaskR.foreach(paths)(path =>
+        isIncluded(path).map((path, _)))
+    } yield
+      flaggedPaths.toStream
+        .filter({ case (_, included) => included })
+        .map({ case (path, _) => path })
+
+  private def localFile(hashService: HashService)(path: Path) = {
+    val file = path.toFile
+    for {
+      sources <- getSources
+      prefix  <- getPrefix
+      hash    <- hashService.hashLocalObject(path)
+      localFile = LocalFile(file,
+                            sources.forPath(path).toFile,
+                            hash,
+                            generateKey(sources, prefix)(path))
+    } yield
+      LocalFiles(localFiles = Stream(localFile),
+                 count = 1,
+                 totalSizeBytes = file.length)
+  }
+
+  private def listFiles(path: Path) =
     for {
       files <- Task(path.toFile.listFiles)
       _ <- Task.when(files == null)(
         Task.fail(new IllegalArgumentException(s"Directory not found $path")))
     } yield Stream(files: _*).map(_.toPath)
+
+  private def isIncluded(path: Path) =
+    for {
+      filters <- getFilters
+    } yield Filter.isIncluded(filters)(path)
+
+  private def pathToLocalFile(hashService: HashService)(path: Path) =
+    localFile(hashService)(path)
 
 }
