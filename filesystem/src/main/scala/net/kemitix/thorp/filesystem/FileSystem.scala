a749b1f5b0971d4de8515375bc7abc21077865bc
Paul Campbell <pcampbell@kemitix.net>
2019-07-30 15:30:12 +0100

    [filesystem] Merge FS object into FileSystem object (#139)

--- filesystem/src/main/scala/net/kemitix/thorp/filesystem/FileSystem.scala
+++ filesystem/src/main/scala/net/kemitix/thorp/filesystem/FileSystem.scala
@@ -76,4 +76,14 @@ object FileSystem {
         fileLinesResult
     }
   }
+
+  final def exists(file: File): ZIO[FileSystem, Throwable, Boolean] =
+    ZIO.accessM(_.filesystem fileExists file)
+
+  final def open(file: File, offset: Long = 0)
+    : TaskR[FileSystem, ZManaged[FileSystem, Throwable, FileInputStream]] =
+    ZIO.accessM(_.filesystem openManagedFileInputStream (file, offset))
+
+  final def lines(file: File): TaskR[FileSystem, List[String]] =
+    ZIO.accessM(_.filesystem fileLines (file))
 }
