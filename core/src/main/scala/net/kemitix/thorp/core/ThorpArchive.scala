696485fa44657b741e0cb2b6e72b118caff16afe
Paul Campbell <pcampbell@kemitix.net>
2019-08-01 14:07:10 +0100

    [core] ThorpArchive Refactoring

--- core/src/main/scala/net/kemitix/thorp/core/ThorpArchive.scala
+++ core/src/main/scala/net/kemitix/thorp/core/ThorpArchive.scala
@@ -3,17 +3,10 @@ package net.kemitix.thorp.core
 import net.kemitix.thorp.config.Config
 import net.kemitix.thorp.console._
 import net.kemitix.thorp.domain.StorageQueueEvent
-import net.kemitix.thorp.domain.StorageQueueEvent.{
-  CopyQueueEvent,
-  DeleteQueueEvent,
-  DoNothingQueueEvent,
-  ErrorQueueEvent,
-  ShutdownQueueEvent,
-  UploadQueueEvent
-}
+import net.kemitix.thorp.domain.StorageQueueEvent._
 import net.kemitix.thorp.domain.Terminal._
 import net.kemitix.thorp.storage.api.Storage
-import zio.TaskR
+import zio.{TaskR, ZIO}
 
 import scala.io.AnsiColor._
 
@@ -30,43 +23,43 @@ trait ThorpArchive {
   ): TaskR[Console with Config, StorageQueueEvent] =
     event match {
       case UploadQueueEvent(remoteKey, _) =>
-        for {
-          batchMode <- Config.batchMode
-          _ <- TaskR.when(batchMode)(
-            Console.putStrLn(s"Uploaded: ${remoteKey.key}"))
-          _ <- TaskR.when(!batchMode)(
-            Console.putStrLn(
-              s"${GREEN}Uploaded:$RESET ${remoteKey.key}$eraseToEndOfScreen"))
-        } yield event
+        ZIO(event) <* logMessage(
+          s"Uploaded: ${remoteKey.key}",
+          s"${GREEN}Uploaded:$RESET ${remoteKey.key}$eraseToEndOfScreen")
       case CopyQueueEvent(sourceKey, targetKey) =>
-        for {
-          batchMode <- Config.batchMode
-          _ <- TaskR.when(batchMode)(
-            Console.putStrLn(s"Copied: ${sourceKey.key} => ${targetKey.key}"))
-          _ <- TaskR.when(!batchMode)(
-            Console.putStrLn(
-              s"${GREEN}Copied:$RESET ${sourceKey.key} => ${targetKey.key}$eraseToEndOfScreen")
-          )
-        } yield event
+        ZIO(event) <* logMessage(
+          s"Copied: ${sourceKey.key} => ${targetKey.key}",
+          s"${GREEN}Copied:$RESET ${sourceKey.key} => ${targetKey.key}$eraseToEndOfScreen")
       case DeleteQueueEvent(remoteKey) =>
-        for {
-          batchMode <- Config.batchMode
-          _         <- TaskR.when(batchMode)(Console.putStrLn(s"Deleted: $remoteKey"))
-          _ <- TaskR.when(!batchMode)(
-            Console.putStrLn(
-              s"${GREEN}Deleted:$RESET ${remoteKey.key}$eraseToEndOfScreen"))
-        } yield event
+        ZIO(event) <* logMessage(
+          s"Deleted: $remoteKey",
+          s"${GREEN}Deleted:$RESET ${remoteKey.key}$eraseToEndOfScreen")
       case ErrorQueueEvent(action, _, e) =>
-        for {
-          batchMode <- Config.batchMode
-          _ <- TaskR.when(batchMode)(
-            Console.putStrLn(
-              s"${action.name} failed: ${action.keys}: ${e.getMessage}"))
-          _ <- TaskR.when(!batchMode)(Console.putStrLn(
-            s"${RED}ERROR:$RESET ${action.name} ${action.keys}: ${e.getMessage}$eraseToEndOfScreen"))
-        } yield event
+        ZIO(event) <* logMessage(
+          s"${action.name} failed: ${action.keys}: ${e.getMessage}",
+          s"${RED}ERROR:$RESET ${action.name} ${action.keys}: ${e.getMessage}$eraseToEndOfScreen")
       case DoNothingQueueEvent(_) => TaskR(event)
       case ShutdownQueueEvent()   => TaskR(event)
     }
+
+  private def logMessage(
+      batchModeMessage: String,
+      normalMessage: String
+  ): ZIO[Console with Config, Nothing, Unit] = {
+    for {
+      batchMode <- Config.batchMode
+      _ <- alternative(batchMode)(
+        Console.putStrLn(batchModeMessage),
+        Console.putStrLn(normalMessage)
+      )
+    } yield ()
+  }
+
+  private def alternative[R, E, A](b: Boolean)(
+      whenTrue: ZIO[R, E, A],
+      whenFalse: ZIO[R, E, A]
+  ) =
+    if (b) whenTrue.const(())
+    else whenFalse.const(())
 
 }
