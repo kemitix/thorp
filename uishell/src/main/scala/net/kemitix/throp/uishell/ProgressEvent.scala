8170897e46d6133e2c62746ce02458a183c4894f
Paul Campbell <pcampbell@kemitix.net>
2019-09-01 18:04:27 +0100

    [uishell] Add stub module

---
+++ uishell/src/main/scala/net/kemitix/throp/uishell/ProgressEvent.scala
@@ -0,0 +1,22 @@
+package net.kemitix.throp.uishell
+
+import net.kemitix.eip.zio.MessageChannel
+import net.kemitix.thorp.config.Config
+import net.kemitix.thorp.console.Console
+import net.kemitix.thorp.filesystem.{FileSystem, Hasher}
+import zio.clock.Clock
+
+sealed trait ProgressEvent
+
+object ProgressEvent {
+  type Env = Console
+  type ProgressSender =
+    MessageChannel.ESender[Config with Clock with Hasher with FileSystem,
+                           Throwable,
+                           ProgressEvent]
+  type ProgressReceiver =
+    MessageChannel.Receiver[ProgressEvent.Env, ProgressEvent]
+  type ProgressChannel = MessageChannel.Channel[Console, ProgressEvent]
+
+  final case class PingEvent() extends ProgressEvent
+}
