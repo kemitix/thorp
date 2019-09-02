c009f1648023a2680dd047c6116daccf189d1419
Paul Campbell <pcampbell@kemitix.net>
2019-09-02 22:11:22 +0100

    [domain] Add interogation queries to RemoteObjects

--- domain/src/main/scala/net/kemitix/thorp/domain/RemoteObjects.scala
+++ domain/src/main/scala/net/kemitix/thorp/domain/RemoteObjects.scala
@@ -1,5 +1,7 @@
 package net.kemitix.thorp.domain
 
+import zio.UIO
+
 import scala.collection.MapView
 
 /**
@@ -11,8 +13,46 @@ final case class RemoteObjects private (
 )
 
 object RemoteObjects {
+
   val empty: RemoteObjects = RemoteObjects(MapView.empty, MapView.empty)
+
   def create(byHash: MapView[MD5Hash, RemoteKey],
              byKey: MapView[RemoteKey, MD5Hash]): RemoteObjects =
     RemoteObjects(byHash, byKey)
+
+  def remoteKeyExists(
+      remoteObjects: RemoteObjects,
+      remoteKey: RemoteKey
+  ): UIO[Boolean] = UIO(remoteObjects.byKey.contains(remoteKey))
+
+  def remoteMatchesLocalFile(
+      remoteObjects: RemoteObjects,
+      localFile: LocalFile
+  ): UIO[Boolean] =
+    UIO(
+      remoteObjects.byKey
+        .get(localFile.remoteKey)
+        .exists(LocalFile.matchesHash(localFile)))
+
+  def remoteHasHash(
+      remoteObjects: RemoteObjects,
+      hashes: Map[HashType, MD5Hash]
+  ): UIO[Boolean] =
+    UIO(hashes.exists {
+      case (_, hash) =>
+        remoteObjects.byHash.contains(hash)
+    })
+
+  def keyForHashes(
+      remoteObjects: RemoteObjects,
+      hashes: Map[HashType, MD5Hash]
+  ): (RemoteKey, MD5Hash) = {
+    val value: Iterable[MD5Hash] = hashes
+      .filter({
+        case (_, hash) => remoteObjects.byHash.contains(hash)
+      })
+      .values
+    value
+  }
+
 }
