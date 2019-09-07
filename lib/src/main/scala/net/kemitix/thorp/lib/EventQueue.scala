dc25d9a2e10787a2d019aebc4b8f0f531355cd5b
Paul Campbell <pcampbell@kemitix.net>
2019-09-07 20:19:11 +0100

    [lib] Remove EventQueue

--- lib/src/main/scala/net/kemitix/thorp/lib/EventQueue.scala
+++
@@ -1,8 +0,0 @@
-package net.kemitix.thorp.lib
-
-import net.kemitix.thorp.domain.StorageQueueEvent
-
-final case class EventQueue(
-    events: LazyList[StorageQueueEvent],
-    bytesInQueue: Long
-)
