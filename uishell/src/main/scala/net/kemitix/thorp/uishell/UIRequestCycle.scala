cb803ba7c1aded3bf9351d92b2f3b25d754f51d6
Paul Campbell <pcampbell@kemitix.net>
2019-09-27 08:02:04 +0100

    [uishell] Display all pending uploads progress

--- uishell/src/main/scala/net/kemitix/thorp/uishell/UIRequestCycle.scala
+++ uishell/src/main/scala/net/kemitix/thorp/uishell/UIRequestCycle.scala
@@ -1,5 +1,7 @@
 package net.kemitix.thorp.uishell
 
+import java.util.concurrent.atomic.AtomicReference
+
 import net.kemitix.thorp.config.Config
 import net.kemitix.thorp.console.Console
 import net.kemitix.thorp.domain.SizeTranslation.sizeInEnglish
@@ -8,27 +10,44 @@ import net.kemitix.thorp.domain.Terminal.{
   eraseToEndOfScreen,
   progressBar
 }
-import net.kemitix.thorp.domain.{LocalFile, Terminal}
-import zio.ZIO
+import net.kemitix.thorp.domain.{LocalFile, RemoteKey, Terminal}
+import zio.{UIO, ZIO}
 
 import scala.io.AnsiColor.{GREEN, RESET}
 
 object UIRequestCycle {
+
+  private case class UploadState(transferred: Long, fileLength: Long)
+
+  private val uploads: AtomicReference[Map[RemoteKey, UploadState]] =
+    new AtomicReference[Map[RemoteKey, UploadState]](Map.empty)
+
   def handle(localFile: LocalFile,
              bytesTransferred: Long,
              index: Int,
              totalBytesSoFar: Long): ZIO[Console with Config, Nothing, Unit] =
     ZIO.when(bytesTransferred < localFile.file.length()) {
-      //TODO create and maintain some state and display that state
-      val fileLength   = localFile.file.length
-      val remoteKey    = localFile.remoteKey.key
       val statusHeight = 3
-      val percent      = f"${(bytesTransferred * 100) / fileLength}%2d"
-      Console.putStrLn(
-        s"${GREEN}Uploading:$RESET $remoteKey$eraseToEndOfScreen\n" +
-          s"$GREEN File:$RESET ($percent%) ${sizeInEnglish(bytesTransferred)} of ${sizeInEnglish(fileLength)}" +
-          s"$eraseLineForward\n" +
-          progressBar(bytesTransferred, fileLength, Terminal.width) +
-          s"${Terminal.cursorPrevLine(statusHeight)}")
+      val current: Map[RemoteKey, UploadState] =
+        uploads.updateAndGet(
+          (m: Map[RemoteKey, UploadState]) =>
+            m.updated(localFile.remoteKey,
+                      UploadState(bytesTransferred, localFile.file.length())))
+      ZIO.foreach(current) { entry =>
+        {
+          val (remoteKey, state) = entry
+          val percent            = f"${(state.transferred * 100) / state.fileLength}%2d"
+          Console.putStrLn(
+            s"${GREEN}Uploading:$RESET ${remoteKey.key}$eraseToEndOfScreen") *>
+            Console.putStrLn(s"$GREEN File:$RESET ($percent%) ${sizeInEnglish(
+              state.transferred)} of ${sizeInEnglish(state.fileLength)}" +
+              s"$eraseLineForward") *> Console.putStrLn(
+            progressBar(state.transferred, state.fileLength, Terminal.width))
+        }
+      } *> Console.putStr(
+        s"${Terminal.cursorPrevLine(statusHeight) * current.size}")
+    } *> ZIO.when(bytesTransferred >= localFile.file.length()) {
+      UIO(uploads.updateAndGet((m: Map[RemoteKey, UploadState]) =>
+        m.removed(localFile.remoteKey))) *> UIO.unit
     }
 }
