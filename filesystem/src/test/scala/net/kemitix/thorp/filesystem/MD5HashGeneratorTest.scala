b635d7ea94f48f33bda2c802031f0b2251c9ee1f
Paul Campbell <pcampbell@kemitix.net>
2020-06-12 20:56:36 +0100

    domain.MD5HashData: convert to Java

--- filesystem/src/test/scala/net/kemitix/thorp/filesystem/MD5HashGeneratorTest.scala
+++ filesystem/src/test/scala/net/kemitix/thorp/filesystem/MD5HashGeneratorTest.scala
@@ -39,15 +39,17 @@ class MD5HashGeneratorTest extends FunSpec {
     describe("read chunks of file") {
       val path = Resource(this, "big-file").toPath
       it("should generate the correct hash for first chunk of the file") {
-        val part1    = BigFile.Part1
-        val expected = Right(part1.hash.hash())
-        val result   = invoke(path, part1.offset, part1.size).map(x => x.hash)
+        val expected = Right(BigFile.Part1.hash.hash())
+        val result =
+          invoke(path, BigFile.Part1.offset, BigFile.Part1.size).map(x =>
+            x.hash)
         assertResult(expected)(result)
       }
       it("should generate the correct hash for second chunk of the file") {
-        val part2    = BigFile.Part2
-        val expected = Right(part2.hash.hash())
-        val result   = invoke(path, part2.offset, part2.size).map(x => x.hash)
+        val expected = Right(BigFile.Part2.hash.hash())
+        val result =
+          invoke(path, BigFile.Part2.offset, BigFile.Part2.size).map(x =>
+            x.hash)
         assertResult(expected)(result)
       }
     }
