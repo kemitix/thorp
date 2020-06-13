2d12b94738794885d104b5ff2473050a22449e07
Paul Campbell <pcampbell@kemitix.net>
2020-06-13 10:33:45 +0100

    domain.HexEncoder: convert test to Java and fix bugs

--- domain/src/main/java/net/kemitix/thorp/domain/HexEncoder.java
+++ domain/src/main/java/net/kemitix/thorp/domain/HexEncoder.java
@@ -3,22 +3,36 @@ package net.kemitix.thorp.domain;
 import java.io.ByteArrayOutputStream;
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class HexEncoder {
 
     public static String encode(byte[] bytes) {
-        return String.format(String.format("%%0%d", bytes.length << 1),
-                new BigInteger(1, bytes));
+        return String.format("%0" + (bytes.length << 1) + "x",
+                new BigInteger(1, bytes))
+                .toUpperCase();
     }
     public static byte[] decode(String hexString) {
         ByteArrayOutputStream bytes =
                 new ByteArrayOutputStream(hexString.length() * 4);
-        Arrays.stream(hexString
+        List<String> hexBytes = Arrays.stream(hexString
                 .replaceAll("[^0-9A-Fa-f]", "")
-                .split(""))
-                .mapToInt(Integer::parseInt)
+                .split("")).collect(Collectors.toList());
+        sliding(hexBytes, 2)
+                .map(hb -> String.join("", hb))
+                .mapToInt(hex -> Integer.parseInt(hex, 16))
                 .forEach(bytes::write);
         return bytes.toByteArray();
     }
 
+    public static <T> Stream<List<T>> sliding(List<T> list, int size) {
+        if(size > list.size())
+            return Stream.empty();
+        return IntStream.range(0, list.size()-size+1)
+                .filter(i -> i % size == 0)
+                .mapToObj(start -> list.subList(start, start+size));
+    }
 }
---
+++ domain/src/test/java/net/kemitix/thorp/domain/HexEncoderTestJ.java
@@ -0,0 +1,29 @@
+package net.kemitix.thorp.domain;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+public class HexEncoderTestJ
+        implements WithAssertions {
+
+    private String text = "test text to encode to hex";
+    private String hex  = "74657374207465787420746F20656E636F646520746F20686578";
+
+    @Test
+    @DisplayName("can round trip a hash decode then encode")
+    public void roundTripDecodeEncode() {
+        String result = HexEncoder.encode(HexEncoder.decode(hex));
+        assertThat(result).isEqualTo(hex);
+    }
+
+    @Test
+    @DisplayName("can round trip a hash encode then decode")
+    public void roundTripEncodeDecode() {
+        byte[] input  = hex.getBytes(StandardCharsets.UTF_8);
+        byte[] result = HexEncoder.decode(HexEncoder.encode(input));
+        assertThat(result).isEqualTo(input);
+    }
+}
