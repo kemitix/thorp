0ec971d630b6018cb46528c069e92d5d84a92f17
Paul Campbell <pcampbell@kemitix.net>
2020-06-13 06:24:19 +0100

    domain.HashType: restrict access to contstructor

--- domain/src/main/java/net/kemitix/thorp/domain/HashType.java
+++ domain/src/main/java/net/kemitix/thorp/domain/HashType.java
@@ -1,8 +1,9 @@
 package net.kemitix.thorp.domain;
 
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HashType {
     public static HashType MD5 = new HashType();
 }
